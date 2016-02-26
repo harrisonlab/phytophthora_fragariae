f267501e7b21a921377747e2f63f8a7364801ff4
Thomas Adams <adamst@emr.ac.uk>
2016-02-26 09:57:52 +0000

    New files for holding blacklace11 and running discovar

---
+++ Discovar_Pf.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#
+# run discovar on P.fragariae genome, change for each strain
+#
+
+export PATH=${PATH}:/home/vicker/programs/discovardenovo-52488-install/bin/
+
+
+Strain=A4
+OUTDIR='/assembly/discovar/P.fragariae/$Strain'
+FRAC=1
+
+READS='/home/groups/harrisonlab/project_files/phytophthora_fragariae/raw_dna/paired/P.fragariae/$Strain/*/*.fastq.gz
+
+mkdir -p ${OUTDIR}
+
+DiscovarDeNovo READS="frac:${FRAC} :: ${READS}" OUT_DIR=${OUTDIR} MEMORY_CHECK=True MAX_MEM_GB=370
