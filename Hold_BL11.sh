d6b5b79b26b09f1d36699412b05c5cf851a291b1
Thomas Adams <thomas.adams@emr.ac.uk>
2016-02-26 09:57:52 +0000

    New files for holding blacklace11 and running discovar

---
+++ Hold_BL11.sh
@@ -0,0 +1,14 @@
+#$ -S /bin/bash
+#$ -l h_vmem=376.0G
+###$ -l mem_free=1.0G
+###$ -l virtual_free=1.0G
+#$ -l h_rt=9999:00:00
+###$ -pe smp 24
+###$ -l h=blacklace11.blacklace
+
+# hold node11 for discovar running directly on the node
+
+while true
+do
+    sleep 10
+done
