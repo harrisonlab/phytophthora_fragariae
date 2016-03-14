eec40c4d106476354d0518ed90a7c00d9a63ebc6
Thomas Adams <adamst@emr.ac.uk>
2016-03-14 11:23:03 +0000

    adds skip_header option to loadtxt

--- filter_BLAST_hits.py
+++ filter_BLAST_hits.py
@@ -1,7 +1,7 @@
 import numpy
 import csv
 
-results = numpy.loadtxt(fname="genome_vs_region.csv", delimiter=",")
+results = numpy.loadtxt(fname="genome_vs_region.csv", delimiter=",", skip_header)
 
 new_col = (float(my_data[:,6])/my_data[:,2])[...,None]
 
