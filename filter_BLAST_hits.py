84e83816ba8a5f59a6c774ce47c8312c52be1964
Thomas Adams <thomas.adams@emr.ac.uk>
2016-03-14 10:59:11 +0000

    Creates full first attempt of fitering results from BLAST

--- filter_BLAST_hits.py
+++ filter_BLAST_hits.py
@@ -1,7 +1,18 @@
 import numpy
+import csv
 
 results = numpy.loadtxt(fname=/home/adamst/blast_output/genome_vs_region.csv)
 
 new_col = (float(my_data[:,6])/my_data[:,2])[...,None]
 
 new_results = numpy.append(results, new_col, 1)
+
+w1 = numpy.where((new_results[0,:]) and ((new_results[13,:] > 0.5) or (new_results[13,:] == 0.5)))
+
+filtered_results = new_results[:, w1]
+
+with open("filtered_contigs.csv", "wb") as f:
+    writer = csv.writer(f)
+    writer.writerows(filtered_results)
+
+filtered_results.shape
