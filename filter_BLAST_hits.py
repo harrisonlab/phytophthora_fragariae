e27d7a421aa4d21a6238ac8059d29298a315cd79
Thomas Adams <adamst@emr.ac.uk>
2016-03-14 17:34:17 +0000

    Corrects order for reducing array command

--- filter_BLAST_hits.py
+++ filter_BLAST_hits.py
@@ -1,5 +1,6 @@
 import numpy
 import csv
+import glob
 
 results = numpy.loadtxt(fname="genome_vs_region_hits.tbl", dtype=object)
 
@@ -16,14 +17,10 @@ new_col = numpy.reshape(new_col_int, (33685, 1))
 
 new_results = numpy.append(results, new_col, 1)
 
-w1 = numpy.where((new_results[:,0]) and ((new_results[:,14] > 0.5) or (new_results[:,14] == 0.5)))
+w12 = numpy.where((new_results[:,14] > 0.5) | (new_results[:,14] == 0.5))[0]
 
-filtered_results = new_results[:, w1]
+filtered_results = new_results[w12, :]
 
 with open("filtered_contigs.csv", "wb") as f:
     writer = csv.writer(f)
     writer.writerows(filtered_results)
-
-filtered_results.shape
-
-names ="ID,Sequence,Sequence_lgth,No_hits,Hit,E-value,Hit_lgth,Per_length,Per_ID,Hit_strand,Hit_start,Hit_end,Hit_seq"
