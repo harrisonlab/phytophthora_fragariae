cdaee73447c3b21717b3f14f4afb22ecdd61386d
Thomas Adams <thomas.adams@emr.ac.uk>
2016-03-14 15:39:32 +0000

    Corrects loadtxt to use tab output and column numbers

--- filter_BLAST_hits.py
+++ filter_BLAST_hits.py
@@ -3,11 +3,11 @@ import csv
 
 results = numpy.loadtxt(fname="genome_vs_region_hits.tbl", dtype=str)
 
-new_col = (float(results[:,6])/float(results[:,2]))[...,None]
+new_col = (float(results[:,3])/float(results[:,13]))[...,None]
 
 new_results = numpy.append(results, new_col, 1)
 
-w1 = numpy.where((new_results[:,0]) and ((new_results[:,13] > 0.5) or (new_results[:,13] == 0.5)))
+w1 = numpy.where((new_results[:,0]) and ((new_results[:,14] > 0.5) or (new_results[:,14] == 0.5)))
 
 filtered_results = new_results[:, w1]
 
