280cd3b0b62afb9d206aa2218241a477aa03c1d0
Thomas Adams <adamst@emr.ac.uk>
2016-03-14 16:45:56 +0000

    Converts new_col to array

--- filter_BLAST_hits.py
+++ filter_BLAST_hits.py
@@ -3,12 +3,14 @@ import csv
 
 results = numpy.loadtxt(fname="genome_vs_region_hits.tbl", dtype=object)
 
-new_col = []
+new_col_list = []
 
 for row in results:
     value_a = float(row[3])
     value_b = float(row[13])
-    new_col.append(value_a / value_b)
+    new_col_list.append(value_a / value_b)
+
+new_col = numpy.asarray(new_col_list)
 
 new_results = numpy.append(results, new_col, 1)
 
