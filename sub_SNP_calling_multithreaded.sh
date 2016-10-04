13aa2a2cffc7afdab6711228c003f9b01297052a
Thomas Adams <thomas.adams@emr.ac.uk>
2016-10-04 17:04:48 +0100

    Copies commands from Maria's directory

---
+++ sub_SNP_calling_multithreaded.sh
@@ -0,0 +1,59 @@
+#$ -S /bin/bash
+#$ -cwd
+#$ -pe smp 6
+#$ -l h_vmem=4G
+#$ -l h=blacklace01.blacklace|blacklace02.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace|blacklace12.blacklace
+
+# Testing parallelisation of GATk HaplotypeCaller - may crash. (It did not! Resulted in 2x speedup)
+# NOTE: this is a haploid organism. For diploid organism, change "ploidy" argument to 2.
+# Changes required in the script:
+# VARIABLES
+# Reference - the genome reference used in read mapping.
+# INSIDE THE GATK command:
+# To specify which BAM mapping files (output from pre_SNP_calling_cleanup.sh, filename ending with "_rg" -> that is, with
+# read group added) are to be used in SNP calling, use the -I argument with full path to each file following after that.
+# Each new BAM file has to be specified after a separate -I
+
+input=/home/sobczm/popgen/input/mappings
+reference=Fus2_canu_contigs_unmasked.fa
+
+filename=$(basename "$reference")
+output="${filename%.*}_temp.vcf"
+output2="${filename%.*}.vcf"
+
+gatk=/home/sobczm/bin/GenomeAnalysisTK-3.6
+
+java -jar $gatk/GenomeAnalysisTK.jar \
+     -R $input/$reference \
+     -T HaplotypeCaller \
+     -ploidy 1 \
+     -nct 6 \
+     --allow_potentially_misencoded_quality_scores \
+     -I $input/125/125_Fus2_canu_contigs_unmasked.fa_aligned_nomulti_proper_sorted_nodup_rg.bam \
+     -I $input/55/55_Fus2_canu_contigs_unmasked.fa_aligned_nomulti_proper_sorted_nodup_rg.bam \
+     -I $input/A1-2/A1-2_Fus2_canu_contigs_unmasked.fa_aligned_nomulti_proper_sorted_nodup_rg.bam \
+     -I $input/A13/A13_Fus2_canu_contigs_unmasked.fa_aligned_nomulti_proper_sorted_nodup_rg.bam \
+     -I $input/A23/A23_Fus2_canu_contigs_unmasked.fa_aligned_nomulti_proper_sorted_nodup_rg.bam \
+     -I $input/A28/A28_Fus2_canu_contigs_unmasked.fa_aligned_nomulti_proper_sorted_nodup_rg.bam \
+     -I $input/CB3/CB3_Fus2_canu_contigs_unmasked.fa_aligned_nomulti_proper_sorted_nodup_rg.bam \
+     -I $input/D2/D2_Fus2_canu_contigs_unmasked.fa_aligned_nomulti_proper_sorted_nodup_rg.bam \
+     -I $input/Fus2/Fus2_Fus2_canu_contigs_unmasked.fa_aligned_nomulti_proper_sorted_nodup_rg.bam \
+     -I $input/HB6/HB6_Fus2_canu_contigs_unmasked.fa_aligned_nomulti_proper_sorted_nodup_rg.bam \
+     -I $input/PG/PG_Fus2_canu_contigs_unmasked.fa_aligned_nomulti_proper_sorted_nodup_rg.bam \
+     -o $output
+
+#Break down complex SNPs into primitive ones with VariantsToAllelicPrimitives
+#This tool will take an MNP (e.g. ACCCA -> TCCCG) and break it up into separate records for each component part (A-T and A->G).
+#This tool modifies only bi-allelic variants.
+
+java -jar $gatk/GenomeAnalysisTK.jar \
+   -T VariantsToAllelicPrimitives \
+   -R $input/$reference \
+   -V $output \
+   -o $output2 \
+
+
+#####################################
+# Notes on GATK parallelisation
+#####################################
+# http://gatkforums.broadinstitute.org/gatk/discussion/1975/how-can-i-use-parallelism-to-make-gatk-tools-run-faster
