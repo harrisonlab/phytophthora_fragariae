894ae85958d5533b0f6957015efff28537f7be73
Thomas Adams <thomas.adams@emr.ac.uk>
2016-11-16 15:23:52 +0000

    Adds colorspace

--- UK3_Pf_venn_diag.r
+++ UK3_Pf_venn_diag.r
@@ -14,6 +14,7 @@
 
 #get config options
 library(optparse)
+library(colorspace)
 library(VennDiagram, lib.loc="/home/armita/R-packages/")
 opt_list = list(
     make_option("--inp", type="character", help="tab seperated file containing matrix of presence of orthogroups"),
