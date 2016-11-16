2d22485a89fc15ecf2ccf48a1dc3880289f07d05
Thomas Adams <adamst@emr.ac.uk>
2016-11-16 15:33:55 +0000

    Copies commands from previous 3-way

--- UK3_Pf_venn_diag.r
+++ UK3_Pf_venn_diag.r
@@ -30,76 +30,146 @@ df1 <- t(orthotabs)
 summary(df1)
 
 
-area1=sum(df1[, 1])
-area2=sum(df1[, 2])
-area3=sum(df1[, 3])
-area4=sum(df1[, 4])
+NOV5=subset(df1, df1[,"A4"] == 0 & df1[,"Nov5"] == 1 & df1[,"Nov27"] == 0 & df1[,"Nov71"] == 0 & df1[,"Bc16"] == 0 & df1[,"Nov9"] == 0 & df1[,"Bc1"] == 0)
+BC1=subset(df1, df1[,"A4"] == 0 & df1[,"Nov5"] == 0 & df1[,"Nov27"] == 0 & df1[,"Nov71"] == 0 & df1[,"Bc16"] == 0 & df1[,"Nov9"] == 0 & df1[,"Bc1"] == 1)
+Others=subset(df1, df1[,"A4"] != 0 & df1[,"Nov5"] == 0 & df1[,"Nov27"] != 0 & df1[,"Nov71"] != 0 & df1[,"Bc16"] != 0 & df1[,"Nov9"] != 0 & df1[,"Bc1"] == 0)
+# orthologs=subset(df1, df1[,"A28"] == 1 & df1[,"CB3"] == 1 & df1[,"PG"] == 1 & df1[,"fo47"] == 1 & df1[,"A1_2"] == 1 & df1[,"Fus2"] == 1 & df1[,"125"] == 1 & df1[,"A23"] == 1 & df1[,"4287"] == 1)
 
-# print(area1, area2, area3, area4)
+# area1=(nrow(nonpath) + nrow(orthologs))
+# area2=(nrow(path) + nrow(orthologs))
+# area3=(nrow(tomato) + nrow(orthologs))
+# area3
+# area2
+# area1
 
-colname1 <- paste(colnames(df1)[1])
-colname2 <- paste(colnames(df1)[2])
-colname3 <- paste(colnames(df1)[3])
-colname4 <- paste(colnames(df1)[4])
 
-label1 <- paste(colname1, ' (', area1, ')', sep="" )
-label2 <- paste(colname2, ' (', area2, ')', sep="" )
-label3 <- paste(colname3, ' (', area3, ')', sep="" )
-label4 <- paste(colname4, ' (', area4, ')', sep="" )
 
-n12=nrow(subset(df1, df1[,1] == 1 & df1[,2] == 1))
-n13=nrow(subset(df1, df1[,1] == 1 & df1[,3] == 1))
-n14=nrow(subset(df1, df1[,1] == 1 & df1[,4] == 1))
-n23=nrow(subset(df1, df1[,2] == 1 & df1[,3] == 1))
-n24=nrow(subset(df1, df1[,2] == 1 & df1[,4] == 1))
-n34=nrow(subset(df1, df1[,3] == 1 & df1[,4] == 1))
-n123=nrow(subset(df1, df1[,1] == 1 & df1[,2] == 1 & df1[,3] == 1))
-n124=nrow(subset(df1, df1[,1] == 1 & df1[,2] == 1 & df1[,4] == 1))
-n134=nrow(subset(df1, df1[,1] == 1 & df1[,3] == 1 & df1[,4] == 1))
-n234=nrow(subset(df1, df1[,2] == 1 & df1[,3] == 1 & df1[,4] == 1))
-n1234=nrow(subset(df1, df1[,1] == 1 & df1[,2] == 1 & df1[,3] == 1 & df1[,4] == 1))
+# Print labels
+# label1 <- paste('Path', ' (', area2, ')', sep="" )
+# label2 <- paste('NonPath', ' (', area1, ')', sep="" )
+# label3 <- paste('FoL', ' (', area3, ')', sep="" )
+
+# Set up labels
+label1 <- paste("NOV-5", sep="" )
+label2 <- paste("BC-1", sep="" )
+label3 <- paste("A4, NOV-27, NOV-71, NOV-9 & BC-16", sep="" )
+
+n123=nrow(subset(df1, df1[,"A4"] != 0 & df1[,"Nov5"] == 1 & df1[,"Nov27"] != 0 & df1[,"Nov71"] != 0 & df1[,"Bc16"] != 0 & df1[,"Nov9"] != 0 & df1[,"Bc1"] == 1))
+n12=n123 + nrow(subset(df1, df1[,"A4"] == 0 & df1[,"Nov5"] == 1 & df1[,"Nov27"] == 0 & df1[,"Nov71"] == 0 & df1[,"Bc16"] == 0 & df1[,"Nov9"] == 0 & df1[,"Bc1"] == 1))
+n13=n123 + nrow(subset(df1, df1[,"A4"] != 0 & df1[,"Nov5"] == 1 & df1[,"Nov27"] != 0 & df1[,"Nov71"] != 0 & df1[,"Bc16"] != 0 & df1[,"Nov9"] != 0 & df1[,"Bc1"] == 0))
+n23=n123 + nrow(subset(df1, df1[,"A4"] != 0 & df1[,"Nov5"] == 0 & df1[,"Nov27"] != 0 & df1[,"Nov71"] != 0 & df1[,"Bc16"] != 0 & df1[,"Nov9"] != 0 & df1[,"Bc1"] == 1))
 summary(n12)
+summary(n13)
+summary(n23)
 summary(n123)
-summary(n1234)
+
+area1=(nrow(NOV5) + (n12 - n123) + (n13 - n123) + n123)
+area2=(nrow(BC1) + (n12 - n123) + (n23 - n123) + n123)
+area3=(nrow(Others) + (n13 - n123) + (n23 - n123) + n123)
+#nrow(nonpath)
+nrow(NOV5)
+nrow(BC1)
+nrow(Others)
+n12
+n13
+n23
+n123
+area1
+#area1 - n12 - n13 + n123
+area2
+area3
+
 pdf(o)
-draw.quad.venn(area1, area2, area3, area4,
-    n12, n13, n14, n23, n24, n34,
-    n123, n124, n134, n234,
-    n1234,
-    category = c(label1, label2, label3, label4),
+draw.triple.venn(area1, area2, area3,
+    n12, n23, n13,
+    n123,
+    category = c(label1, label2, label3),
 #    rep("", 4),
-    lwd = rep(2, 4), lty = rep("solid", 4),
-    col = rep("black", 4), fill = NULL, alpha = rep(0.5, 4),
-    label.col = rep("black", 15), cex = rep(1, 15), fontface = rep("plain", 15),
-    fontfamily = rep("serif", 15), cat.pos = c(-15, 15, 0, 0),
-    cat.dist = c(0.22, 0.22, 0.11, 0.11), cat.col = rep("black", 4),
-    cat.cex = rep(1, 4), cat.fontface = rep("plain", 4),
-    cat.fontfamily = rep("serif", 4), cat.just = rep(list(c(0.5, 0.5)), 4),
-    rotation.degree = 0, rotation.centre = c(0.5, 0.5)
+    rotation = 1,
+    reverse = FALSE,
+    lwd = rep(2, 3),
+    lty = rep("solid", 3),
+    col = rep("black", 3),
+    fill = c(rainbow_hcl(3)),
+    alpha = rep(0.5, 3),
+    label.col = rep("black", 7),
+    cex = rep(1, 7),
+    fontface = rep("plain", 7),
+    fontfamily = rep("serif", 7),
+    cat.pos = c(-40, 40, 180),
+    cat.dist = c(0.05, 0.05, 0.025),
+    cat.col = rep("black", 3),
+    cat.cex = rep(1, 3),
+    cat.fontface = rep("plain", 3),
+    cat.fontfamily = rep("serif", 3),
+    cat.just = list(c(0.5, 1), c(0.5, 1), c(0.5, 0)),
+    cat.default.pos = "outer",
+    cat.prompts = FALSE,
+    rotation.degree = 0,
+    rotation.centre = c(0.5, 0.5),
+    ind = TRUE, sep.dist = 0.05, offset = 0,
     )
+
+
 dev.off()
-singles = df1[grepl("single*", rownames(df1)), ]
-uniq_1=sum(singles[, 1])
-uniq_2=sum(singles[, 2])
-uniq_3=sum(singles[, 3])
-uniq_4=sum(singles[, 4])
-orthogroups = df1[grepl("orthogroup*", rownames(df1)), ]
-inpara_1 = sum(orthogroups[,1] == 1 & orthogroups[,2] == 0 & orthogroups[,3] == 0 & orthogroups[,4] == 0)
-inpara_2 = sum(orthogroups[,1] == 0 & orthogroups[,2] == 1 & orthogroups[,3] == 0 & orthogroups[,4] == 0)
-inpara_3 = sum(orthogroups[,1] == 0 & orthogroups[,2] == 0 & orthogroups[,3] == 1 & orthogroups[,4] == 0)
-inpara_4 = sum(orthogroups[,1] == 0 & orthogroups[,2] == 0 & orthogroups[,3] == 0 & orthogroups[,4] == 1)
-label1
-uniq_1
-inpara_1
-label2
-uniq_2
-inpara_2
-label3
-uniq_3
-inpara_3
-label4
-uniq_4
-inpara_4
+# singles = df1[grepl("single*", rownames(df1)), ]
+# print("A28")
+# total_1 = nrow(subset (df1, df1[,"A28"] == 1))
+# missing_1 = (total_1 - area2)
+# uniq_1=sum(singles[, "A28"])
+# paste('The total number of orthogroups and singleton genes in this isolate: ', total_1)
+# paste('The total number of orthogroups and singleton genes not in the venn diagram: ', missing_1)
+# paste('The total number of singleton genes not in the venn diagram: ', uniq_1)
+# print("CB3")
+# total_2 = nrow(subset (df1, df1[,"CB3"] == 1))
+# missing_2 = (total_2 - area2)
+# uniq_2=sum(singles[, "CB3"])
+# paste('The total number of orthogroups and singleton genes in this isolate: ', total_2)
+# paste('The total number of orthogroups and singleton genes not in the venn diagram: ', missing_2)
+# paste('The total number of singleton genes not in the venn diagram: ', uniq_2)
+# print("PG")
+# total_3 = nrow(subset (df1, df1[,"PG"] == 1))
+# missing_3 = (total_3 - area2)
+# uniq_3=sum(singles[, "PG"])
+# paste('The total number of orthogroups and singleton genes in this isolate: ', total_3)
+# paste('The total number of orthogroups and singleton genes not in the venn diagram: ', missing_3)
+# paste('The total number of singleton genes not in the venn diagram: ', uniq_3)
+# print("Fus2")
+# total_4 = nrow(subset (df1, df1[,"Fus2"] == 1))
+# missing_4 = (total_4 - area1)
+# uniq_4=sum(singles[, "Fus2"])
+# paste('The total number of orthogroups and singleton genes in this isolate: ', total_4)
+# paste('The total number of orthogroups and singleton genes not in the venn diagram: ', missing_4)
+# paste('The total number of singleton genes not in the venn diagram: ', uniq_4)
+# print("125")
+# total_5 = nrow(subset (df1, df1[,"125"] == 1))
+# missing_5 = (total_5 - area1)
+# uniq_5=sum(singles[, "125"])
+# paste('The total number of orthogroups and singleton genes in this isolate: ', total_5)
+# paste('The total number of orthogroups and singleton genes not in the venn diagram: ', missing_5)
+# paste('The total number of singleton genes not in the venn diagram: ', uniq_5)
+# print("A23")
+# total_6 = nrow(subset (df1, df1[,"A23"] == 1))
+# missing_6 = (total_6 - area1)
+# uniq_6=sum(singles[, "A23"])
+# paste('The total number of orthogroups and singleton genes in this isolate: ', total_6)
+# paste('The total number of orthogroups and singleton genes not in the venn diagram: ', missing_6)
+# paste('The total number of singleton genes not in the venn diagram: ', uniq_6)
+# print("4287")
+# total_7 = nrow(subset (df1, df1[,"4287"] == 1))
+# missing_7 = (total_6 - area3)
+# uniq_7=sum(singles[, "4287"])
+# paste('The total number of orthogroups and singleton genes in this isolate: ', total_6)
+# paste('The total number of orthogroups and singleton genes not in the venn diagram: ', missing_6)
+# paste('The total number of singleton genes not in the venn diagram: ', uniq_6)
+
+#inpara_2 = sum(orthogroups[,"A28"] == 0 & orthogroups[,"CB3"] == 1)
+#label1
+#uniq_1
+#inpara_1
+#label2
+#uniq_2
+#inpara_2
 
 warnings()
 q()
