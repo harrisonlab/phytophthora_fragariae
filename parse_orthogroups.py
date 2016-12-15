e4e515ebb57b4f0cd0883dd26b3f493c268d7f79
Thomas Adams <adamst@emr.ac.uk>
2016-12-15 16:48:02 +0000

    Adds commands to attempt to count nuber of things in a cell

--- parse_orthogroups.py
+++ parse_orthogroups.py
@@ -1,1 +1,8 @@
 import openpyxl
+
+wb = openpyxl.load_workbook('orthomcl_results_rearranged.xlsx')
+sheet = wb.get_sheet_by_name('All_strains')
+
+for i in range(1,1,1):
+    l = print(i, sheet.cell(row=i, column=2).value)
+    print len(l)
