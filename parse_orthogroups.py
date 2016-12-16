4c3b740ddd9217db6145cfdec9d827e3133f5cf6
Thomas Adams <adamst@emr.ac.uk>
2016-12-16 17:09:36 +0000

    Edits commands to count numbers of genes

--- parse_orthogroups.py
+++ parse_orthogroups.py
@@ -1,4 +1,10 @@
 import openpyxl
 
-wb = load_workbook('orthomcl_results_rearranged.xlsx')
+wb = openpyxl.load_workbook('orthomcl_results_rearranged.xlsx')
 sheet = wb.get_sheet_by_name('All_strains')
+
+for row in sheet.iter_rows(min_row=19784, min_col=2, max_col=12, max_row=19784):
+    for cell in row:
+        test = str((cell).value)
+        search = "t1"
+        print test.count(search)
