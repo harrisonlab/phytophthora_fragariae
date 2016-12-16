a1360f8ac6397b2a15a7018efe23281fbb1c3961
Thomas Adams <adamst@emr.ac.uk>
2016-12-16 11:07:24 +0000

    Edits commands to test attempt to call entire column

--- parse_orthogroups.py
+++ parse_orthogroups.py
@@ -3,16 +3,18 @@ import openpyxl
 wb = openpyxl.load_workbook('orthomcl_results_rearranged.xlsx')
 sheet = wb.get_sheet_by_name('All_strains')
 
-for row in range(1, sheet.max_row +1):
-    Orthogroups = sheet['A' + str(row)].value
-    A4 = sheet['B' + str(row)].value
-    Bc1 = sheet['C' + str(row)].value
-    Bc16 = sheet['D' + str(row)].value
-    Bc23 = sheet['E' + str(row)].value
-    Nov27 = sheet['F' + str(row)].value
-    Nov5 = sheet['G' + str(row)].value
-    Nov71 = sheet['H' + str(row)].value
-    Nov77 = sheet['I' + str(row)].value
-    Nov9 = sheet['J' + str(row)].value
-    ONT3 = sheet['K' + str(row)].value
-    SCRP25_v2 = sheet['L' + str(row)].value
+for row in range(0, sheet.max_row +1):
+    Orthogroups = sheet['A' + str(row)]
+    A4 = sheet['B']
+    Bc1 = sheet['C' + str(row)]
+    Bc16 = sheet['D' + str(row)]
+    Bc23 = sheet['E' + str(row)]
+    Nov27 = sheet['F' + str(row)]
+    Nov5 = sheet['G' + str(row)]
+    Nov71 = sheet['H' + str(row)]
+    Nov77 = sheet['I' + str(row)]
+    Nov9 = sheet['J' + str(row)]
+    ONT3 = sheet['K' + str(row)]
+    SCRP25_v2 = sheet['L' + str(row)]
+
+print(A4)
