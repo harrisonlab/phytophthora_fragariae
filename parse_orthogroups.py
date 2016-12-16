2277a4b65c541b798a525c6b7c6e85ed26abaa1c
Thomas Adams <thomas.adams@emr.ac.uk>
2016-12-16 10:30:37 +0000

    Edits commands to iterate over rows

--- parse_orthogroups.py
+++ parse_orthogroups.py
@@ -3,6 +3,16 @@ import openpyxl
 wb = openpyxl.load_workbook('orthomcl_results_rearranged.xlsx')
 sheet = wb.get_sheet_by_name('All_strains')
 
-for i in range(1,1,1):
-    l = print(i, sheet.cell(row=i, column=2).value)
-    print len(l)
+for row in range(1, sheet.max_row +1):
+    Orthogroups = sheet['A' + str(row)].value
+    A4 = sheet['B' + str(row)].value
+    Bc1 = sheet['C' + str(row)].value
+    Bc16 = sheet['D' + str(row)].value
+    Bc23 = sheet['E' + str(row)].value
+    Nov27 = sheet['F' + str(row)].value
+    Nov5 = sheet['G' + str(row)].value
+    Nov71 = sheet['H' + str(row)].value
+    Nov77 = sheet['I' + str(row)].value
+    Nov9 = sheet['J' + str(row)].value
+    ONT3 = sheet['K' + str(row)].value
+    SCRP25_v2 = sheet['L' + str(row)].value
