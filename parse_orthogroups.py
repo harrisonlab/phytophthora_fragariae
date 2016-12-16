93e2ea126cea57229d884c9855645e1951fd8072
Thomas Adams <thomas.adams@emr.ac.uk>
2016-12-16 12:05:31 +0000

    Removes testing commands from script

--- parse_orthogroups.py
+++ parse_orthogroups.py
@@ -3,9 +3,9 @@ import openpyxl
 wb = openpyxl.load_workbook('orthomcl_results_rearranged.xlsx')
 sheet = wb.get_sheet_by_name('All_strains')
 
-for row in range(0, sheet.max_row +1):
+for row in range(1, sheet.max_row +1):
     Orthogroups = sheet['A' + str(row)]
-    A4 = sheet['B']
+    A4 = sheet['B' + str(row)]
     Bc1 = sheet['C' + str(row)]
     Bc16 = sheet['D' + str(row)]
     Bc23 = sheet['E' + str(row)]
