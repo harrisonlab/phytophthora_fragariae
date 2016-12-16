2f729dc9092303f661747f66c5930ddb56dec92a
Thomas Adams <thomas.adams@emr.ac.uk>
2016-12-16 17:10:27 +0000

    Edits min and max rows

--- parse_orthogroups.py
+++ parse_orthogroups.py
@@ -3,7 +3,7 @@ import openpyxl
 wb = openpyxl.load_workbook('orthomcl_results_rearranged.xlsx')
 sheet = wb.get_sheet_by_name('All_strains')
 
-for row in sheet.iter_rows(min_row=19784, min_col=2, max_col=12, max_row=19784):
+for row in sheet.iter_rows(min_row=2, min_col=2, max_col=12, max_row=19952):
     for cell in row:
         test = str((cell).value)
         search = "t1"
