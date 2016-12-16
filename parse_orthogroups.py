30b9e85774f48a7da325ed99f55747961996328d
Thomas Adams <adamst@emr.ac.uk>
2016-12-16 14:49:46 +0000

    Removes previous attempt to call columns

--- parse_orthogroups.py
+++ parse_orthogroups.py
@@ -2,19 +2,3 @@ import openpyxl
 
 wb = load_workbook('orthomcl_results_rearranged.xlsx')
 sheet = wb.get_sheet_by_name('All_strains')
-
-for row in range(1, sheet.max_row +1):
-    Orthogroups = sheet['A' + str(row)]
-    A4 = sheet['B' + str(row)]
-    Bc1 = sheet['C' + str(row)]
-    Bc16 = sheet['D' + str(row)]
-    Bc23 = sheet['E' + str(row)]
-    Nov27 = sheet['F' + str(row)]
-    Nov5 = sheet['G' + str(row)]
-    Nov71 = sheet['H' + str(row)]
-    Nov77 = sheet['I' + str(row)]
-    Nov9 = sheet['J' + str(row)]
-    ONT3 = sheet['K' + str(row)]
-    SCRP25_v2 = sheet['L' + str(row)]
-
-print(A4)
