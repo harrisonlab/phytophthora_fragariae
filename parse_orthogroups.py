fa7dc806fc805d874d5e2a08da5bdb082b41e017
Thomas Adams <thomas.adams@emr.ac.uk>
2016-12-19 13:43:26 +0000

    Adds commands to deal with input files and output directory

--- parse_orthogroups.py
+++ parse_orthogroups.py
@@ -13,3 +13,11 @@ import re
 # Step 1
 # Import variables & load input files
 #-----------------------------------------------------
+
+ap = argparse.ArgumentParser()
+ap.add_argument('--orthogroups',required=True,type=str,help='text file output of OrthoMCL orthogroups')
+ap.add_arguments('--out_dir',required=True,type=str,help='the directory where the count tble is output to')
+conf = ap.parse_args()
+
+with open(conf.orthogroups) as f:
+    ortho_lines = f.readlines()
