1e6a55e1e52a90bbcf2bd48672a0fecfaf7064de
Thomas Adams <thomas.adams@emr.ac.uk>
2016-12-19 13:34:42 +0000

    Adds commands to import libraries

--- parse_orthogroups.py
+++ parse_orthogroups.py
@@ -3,3 +3,8 @@
 '''
 This script uses an orthology group txt file output from OrthoMCL to create a count table for the number of genes per strain in each orthogroup
 '''
+
+from sets import Set
+import sys,argparse
+from collections import defaultdict
+import re
