adce79601b689c08b09ebe73599479c7c175e786
Thomas Adams <adamst@emr.ac.uk>
2016-12-19 16:19:05 +0000

    Corrects commands to create an array

--- parse_orthogroups.py
+++ parse_orthogroups.py
@@ -49,7 +49,7 @@ for line in ortho_lines:
 keys = []
 sorted_keys = []
 keys = ortho_dict.keys()
-a = numpy.array(["Orthogroup", "A4", "Bc1", "Bc16", "Bc23", "Nov27", "Nov5", "Nov71", "Nov77", "Nov9", "ONT3", "SCRP245_v2"])
+a = numpy.array([["Orthogroup", "A4", "Bc1", "Bc16", "Bc23", "Nov27", "Nov5", "Nov71", "Nov77", "Nov9", "ONT3", "SCRP245_v2"]])
 
 keys.sort(key=int)
 ortho_list = []
