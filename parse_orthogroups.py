21c72efed4ff1101cdd7c184818bd33429396074
Thomas Adams <thomas.adams@emr.ac.uk>
2016-12-19 14:49:51 +0000

    Adds commands to count occurence of each strain by group

--- parse_orthogroups.py
+++ parse_orthogroups.py
@@ -43,3 +43,24 @@ for line in ortho_lines:
 # Create counts for each entry and write to a table
 #-----------------------------------------------------
 # For each strain in each orthogroup, add the constituent genes to a set. Count the number of genes in this set and print to a table.
+
+keys = []
+sorted_keys = []
+keys = ortho_dict.keys()
+
+keys.sort(key=int)
+ortho_list = []
+for group_name in keys:
+    print ("orthogroup" + str(group_name))
+    print ("Counting genes in orthogroup: " + str(group_name))
+    A4 = ortho_dict[group_name].count(A4|)
+    Bc1 = ortho_dict[group_name].count(Bc1|)
+    Bc16 = ortho_dict[group_name].count(Bc16|)
+    Bc23 = ortho_dict[group_name].count(Bc23|)
+    Nov5 = ortho_dict[group_name].count(Nov5|)
+    Nov27 = ortho_dict[group_name].count(Nov27|)
+    Nov9 = ortho_dict[group_name].count(Nov9|)
+    Nov71 = ortho_dict[group_name].count(Nov71|)
+    Nov77 = ortho_dict[group_name].count(Nov77|)
+    ONT3 = ortho_dict[group_name].count(ONT3|)
+    SCRP245 = ortho_dict[group_name].count(SCRP245_v2|)
