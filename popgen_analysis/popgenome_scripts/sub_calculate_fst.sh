21e63b4de0945e1a4fda1a7eadc53b42d781bbe4
Thomas Adams <adamst@emr.ac.uk>
2017-01-03 16:34:19 +0000

    Copies files and edits shell scripts to correctly reference this folder

---
+++ popgen_analysis/popgenome_scripts/sub_calculate_fst.sh
@@ -0,0 +1,9 @@
+#$ -S /bin/bash
+#$ -cwd
+#$ -pe smp 6
+#$ -l h_vmem=4G
+#$ -l h=blacklace01.blacklace|blacklace02.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace|blacklace12.blacklace
+
+scripts=/home/adamst/git_repos/scripts/phytophthora_fragariae/popgen_analysis/popgenome_scripts
+
+Rscript --vanilla $scripts/calculate_fst.R
