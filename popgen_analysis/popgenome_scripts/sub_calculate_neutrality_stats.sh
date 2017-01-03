f6a6a7d47e3ace93fad21cfb5cf21216bd5f6e4b
Thomas Adams <thomas.adams@emr.ac.uk>
2017-01-03 16:34:19 +0000

    Copies files and edits shell scripts to correctly reference this folder

---
+++ popgen_analysis/popgenome_scripts/sub_calculate_neutrality_stats.sh
@@ -0,0 +1,9 @@
+#$ -S /bin/bash
+#$ -cwd
+#$ -pe smp 1
+#$ -l h_vmem=8G
+#$ -l h=blacklace01.blacklace|blacklace02.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace|blacklace12.blacklace
+
+scripts=/home/adamst/git_repos/scripts/phytophthora_fragariae/popgen_analysis/popgenome_scripts
+
+Rscript --vanilla $scripts/calculate_neutrality_stats.R
