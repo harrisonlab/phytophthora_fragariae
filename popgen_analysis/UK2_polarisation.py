c9bdc3c5716985c110314035f36bb76ed396ce98
Thomas Adams <thomas.adams@emr.ac.uk>
2017-04-19 11:53:10 +0100

    Adds commands to deal with arguments

--- popgen_analysis/UK2_polarisation.py
+++ popgen_analysis/UK2_polarisation.py
@@ -2,3 +2,7 @@ import argparse
 import numpy
 import csv
 import glob
+
+parser = argparse.ArgumentParser(description='Choose a tbl file to analyse')
+parser.add_argument('tbl',required=True,type=str,help='tbl file to be analysed by script')
+conf = parser.parse_args()
