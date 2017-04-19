c28141391c0e51514bb497cc2a9e0b4630b71ab2
Thomas Adams <thomas.adams@emr.ac.uk>
2017-04-19 11:54:11 +0100

    Edits tbl option

--- popgen_analysis/UK2_polarisation.py
+++ popgen_analysis/UK2_polarisation.py
@@ -4,5 +4,5 @@ import csv
 import glob
 
 parser = argparse.ArgumentParser(description='Choose a tbl file to analyse')
-parser.add_argument('tbl',required=True,type=str,help='tbl file to be analysed by script')
+parser.add_argument('--tbl',required=True,type=str,help='tbl file to be analysed by script')
 conf = parser.parse_args()
