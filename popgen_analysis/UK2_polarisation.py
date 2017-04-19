60f2be87bd180e80e8d8fabe8a6bdb21e0ed8334
Thomas Adams <adamst@emr.ac.uk>
2017-04-19 11:56:54 +0100

    Adds command to import tbl file

--- popgen_analysis/UK2_polarisation.py
+++ popgen_analysis/UK2_polarisation.py
@@ -6,3 +6,5 @@ import glob
 parser = argparse.ArgumentParser(description='Choose a tbl file to analyse')
 parser.add_argument('--tbl',required=True,type=str,help='tbl file to be analysed by script')
 conf = parser.parse_args()
+
+Inital_tbl = numpy.loadtxt(fname="conf.tbl", dtype=object)
