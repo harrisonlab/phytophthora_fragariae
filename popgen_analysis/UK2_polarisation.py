e5feeb5efe9c88cff5420b27ffa9524e2274f71a
Thomas Adams <thomas.adams@emr.ac.uk>
2017-04-19 13:27:45 +0100

    Comments out further cut downs and prints tbl2, to test if idea works

--- popgen_analysis/UK2_polarisation.py
+++ popgen_analysis/UK2_polarisation.py
@@ -11,20 +11,20 @@ Inital_tbl = numpy.loadtxt(fname="Parsed_Polarising_95m_contigs_unmasked.tbl", d
 #Cut down table to give equal variants
 
 tbl2 = numpy.where(Inital_tbl[:,7] == Inital_tbl[:,5])
-tbl3 = numpy.where(tbl2[:,7] == tbl2[:,12])
-tbl4 = numpy.where(tbl3[:,7] == tbl3[:,13])
-tbl5 = numpy.where(tbl4[:,7] == tbl4[:,14])
-
-#Further cut down for different variants
-
-tbl6 = numpy.where(tbl5[:,7] != tbl5[:,6])
-tbl7 = numpy.where(tbl6[:,7] != tbl6[:,8])
-tbl8 = numpy.where(tbl7[:,7] != tbl7[:,9])
-tbl9 = numpy.where(tbl8[:,7] != tbl8[:,10])
-tbl10 = numpy.where(tbl9[:,7] != tbl9[:,11])
+# tbl3 = numpy.where(tbl2[:,7] == tbl2[:,12])
+# tbl4 = numpy.where(tbl3[:,7] == tbl3[:,13])
+# tbl5 = numpy.where(tbl4[:,7] == tbl4[:,14])
+#
+# #Further cut down for different variants
+#
+# tbl6 = numpy.where(tbl5[:,7] != tbl5[:,6])
+# tbl7 = numpy.where(tbl6[:,7] != tbl6[:,8])
+# tbl8 = numpy.where(tbl7[:,7] != tbl7[:,9])
+# tbl9 = numpy.where(tbl8[:,7] != tbl8[:,10])
+# tbl10 = numpy.where(tbl9[:,7] != tbl9[:,11])
 
 #Print resulting table to a .csv
 
 with open("UK2_polarisation.csv", "wb") as f:
     writer = csv.writer(f)
-    writer.writerows(tbl10)
+    writer.writerows(tbl2)
