7302e183923297e52225c4d43aa5c5e5f0754fa3
Thomas Adams <adamst@emr.ac.uk>
2017-04-19 14:14:06 +0100

    Edits commands for breaking down tables differently

--- popgen_analysis/UK2_polarisation.py
+++ popgen_analysis/UK2_polarisation.py
@@ -10,18 +10,18 @@ Inital_tbl = numpy.loadtxt(fname="Parsed_Polarising_95m_contigs_unmasked.tbl", d
 
 #Cut down table to give equal variants
 
-tbl2 = numpy.where(Inital_tbl[:,7] == Inital_tbl[:,5])
-# tbl3 = numpy.where(tbl2[:,7] == tbl2[:,12])
-# tbl4 = numpy.where(tbl3[:,7] == tbl3[:,13])
-# tbl5 = numpy.where(tbl4[:,7] == tbl4[:,14])
-#
-# #Further cut down for different variants
-#
-# tbl6 = numpy.where(tbl5[:,7] != tbl5[:,6])
-# tbl7 = numpy.where(tbl6[:,7] != tbl6[:,8])
-# tbl8 = numpy.where(tbl7[:,7] != tbl7[:,9])
-# tbl9 = numpy.where(tbl8[:,7] != tbl8[:,10])
-# tbl10 = numpy.where(tbl9[:,7] != tbl9[:,11])
+tbl2 = Inital_tbl[Inital_tbl[:,7] == Inital_tbl[:,5]]
+tbl3 = tbl2[tbl2[:,7] == tbl2[:,12]]
+tbl4 = tbl3[tbl3[:,7] == tbl3[:,13]]
+tbl5 = tbl4[tbl4[:,7] == tbl4[:,14]]
+
+#Further cut down for different variants
+
+tbl6 = tbl5[tbl5[:,7] != tbl5[:,6]]
+tbl7 = tbl6[tbl6[:,7] != tbl6[:,8]]
+tbl8 = tbl7[tbl7[:,7] != tbl7[:,9]]
+tbl9 = tbl8[tbl8[:,7] != tbl8[:,10]]
+tbl10 = tbl9[tbl9[:,7] != tbl9[:,11]]
 
 #Print resulting table to a .csv
 
