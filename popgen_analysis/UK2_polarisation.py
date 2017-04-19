79c996cb5d78833c444296c00b152907a184b711
Thomas Adams <thomas.adams@emr.ac.uk>
2017-04-19 13:21:17 +0100

    Adds commands to write results to a .csv

--- popgen_analysis/UK2_polarisation.py
+++ popgen_analysis/UK2_polarisation.py
@@ -29,3 +29,9 @@ tbl7 = numpy.where(tbl6[:,7] != tbl6[:,8])
 tbl8 = numpy.where(tbl7[:,7] != tbl7[:,9])
 tbl9 = numpy.where(tbl8[:,7] != tbl8[:,10])
 tbl10 = numpy.where(tbl9[:,7] != tbl9[:,11])
+
+#Print resulting table to a .csv
+
+with open("UK2_polarisation.csv", "wb") as f:
+    writer = csv.writer(f)
+    writer.writerows(tbl10)
