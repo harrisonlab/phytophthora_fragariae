87ea9f12590b0bc73d979a5004c8a67a52fd48a4
Thomas Adams <adamst@emr.ac.uk>
2017-04-24 14:42:46 +0100

    Edits commands to correct orthology_tbl variable name

--- orthology_counts.py
+++ orthology_counts.py
@@ -6,7 +6,7 @@ import glob
 
 #Import tsv into numpy
 
-Othology_tbl = numpy.loadtxt(fname="All_Strains_count_table.tsv", dtype=object, delimiter='\t')
+Orthology_tbl = numpy.loadtxt(fname="All_Strains_count_table.tsv", dtype=object, delimiter='\t')
 
 #Run calculations for UK1
 
