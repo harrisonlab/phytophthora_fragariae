7315f8759218aeb3c2ab1da5aabc8d7bda109435
Thomas Adams <thomas.adams@emr.ac.uk>
2017-04-25 11:54:22 +0100

    Adds commands to print files for UK2 and UK3

--- orthology_counts.py
+++ orthology_counts.py
@@ -18,24 +18,26 @@ with open("UK1_expanded.txt", "w") as f:
     for o in hits:
         f.write(str(o) + '\n')
 
-# #Run calculations for UK2
-#
-# hits = []
-#
-# for row in Orthology_tbl:
-#     if float(row[1]) > float(row[2]) and float(row[1]) > float(row[5]) and float(row[1]) > float(row[6]) and float(row[1]) > float(row[7]) and float(row[1]) > float(row[9]) and float(row[3]) > float(row[2]) and float(row[3]) > float(row[5]) and float(row[3]) > float(row[6]) and float(row[3]) > float(row[7]) and float(row[3]) > float(row[9]) and float(row[1]) > 0 and float(row[3]) > 0:
-#         hits.append(row[0])
-#
-# print 'UK2'
-# print hits
-#
-# #Run calculations for UK3
-#
-# hits = []
-#
-# for row in Orthology_tbl:
-#     if float(row[5]) > float(row[1]) and float(row[5]) > float(row[2]) and float(row[5]) > float(row[3]) and float(row[5]) > float(row[6]) and float(row[7]) > float(row[1]) and float(row[7]) > float(row[2]) and float(row[7]) > float(row[3]) and float(row[7]) > float(row[6]) and float(row[9]) > float(row[1]) and float(row[9]) > float(row[2]) and float(row[9]) > float(row[3]) and float(row[9]) > float(row[6]) and float(row[5]) > 0 and float(row[7]) > 0 and float(row[9]) > 0:
-#         hits.append(row[0])
-#
-# print 'UK3'
-# print hits
+#Run calculations for UK2
+
+hits = []
+
+for row in Orthology_tbl:
+    if float(row[1]) > float(row[2]) and float(row[1]) > float(row[5]) and float(row[1]) > float(row[6]) and float(row[1]) > float(row[7]) and float(row[1]) > float(row[9]) and float(row[3]) > float(row[2]) and float(row[3]) > float(row[5]) and float(row[3]) > float(row[6]) and float(row[3]) > float(row[7]) and float(row[3]) > float(row[9]) and float(row[1]) > 0 and float(row[3]) > 0:
+        hits.append(row[0])
+
+with open("UK2_expanded.txt", "w") as f:
+    for o in hits:
+        f.write(str(o) + '\n')
+
+#Run calculations for UK3
+
+hits = []
+
+for row in Orthology_tbl:
+    if float(row[5]) > float(row[1]) and float(row[5]) > float(row[2]) and float(row[5]) > float(row[3]) and float(row[5]) > float(row[6]) and float(row[7]) > float(row[1]) and float(row[7]) > float(row[2]) and float(row[7]) > float(row[3]) and float(row[7]) > float(row[6]) and float(row[9]) > float(row[1]) and float(row[9]) > float(row[2]) and float(row[9]) > float(row[3]) and float(row[9]) > float(row[6]) and float(row[5]) > 0 and float(row[7]) > 0 and float(row[9]) > 0:
+        hits.append(row[0])
+
+with open("UK3_expanded.txt", "w") as f:
+    for o in hits:
+        f.write(str(o) + '\n')
