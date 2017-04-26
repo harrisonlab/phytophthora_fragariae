9c43c0930d01c7da27a24bfde134ebbf273d46d9
Thomas Adams <adamst@emr.ac.uk>
2017-04-26 10:54:16 +0100

    Copies commands from FALON docs

---
+++ FALCON/Plot_ovlp_stats.R
@@ -0,0 +1,13 @@
+library(ggplot2)
+o<-read.table("ovlp.stats", header=F)
+colnames(o)<-c("pread","length","fivePrimeOvlps","threePrimeOvlps")
+
+pdf(file="OvlpHist.pdf", width=11, height=5)
+par(oma=c(3,3,2,0), cex=1.6, las=1, mar=c(4,4,2,2), mfrow=c(1,2))
+hist(o$fivePrimeOvlps, breaks=100,
+        xlab="number of overlaps between preads",
+        ylab="count", main="Five Prime")
+hist(o$threePrimeOvlps, breaks=100,
+        xlab="number of overlaps between preads",
+        ylab="count", main="Three Prime")
+dev.off()
