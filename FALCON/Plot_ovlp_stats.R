4e95dd8d5555a9be40f7cedb0085aae6eeb74028
Thomas Adams <thomas.adams@emr.ac.uk>
2017-05-02 17:46:53 +0100

    makes changes to plot on log scale

--- FALCON/Plot_ovlp_stats.R
+++ FALCON/Plot_ovlp_stats.R
@@ -4,10 +4,10 @@ colnames(o)<-c("pread","length","fivePrimeOvlps","threePrimeOvlps")
 
 pdf(file="OvlpHist.pdf", width=11, height=5)
 par(oma=c(3,3,2,0), cex=1.6, las=1, mar=c(4,4,2,2), mfrow=c(1,2))
-hist(o$fivePrimeOvlps, breaks=100,
-        xlab="number of overlaps between preads", xlim=c(0,200),
+hist(log(o$fivePrimeOvlps), breaks=500,
+        xlab="number of overlaps between preads",
         ylab="count", main="Five Prime")
-hist(o$threePrimeOvlps, breaks=100,
-        xlab="number of overlaps between preads", xlim=c(0,200),
+hist(log(o$threePrimeOvlps), breaks=500,
+        xlab="number of overlaps between preads",
         ylab="count", main="Three Prime")
 dev.off()
