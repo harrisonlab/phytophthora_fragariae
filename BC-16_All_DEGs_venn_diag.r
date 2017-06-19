f7bcbcc18c55c6111ad0a795650b7a66d3b70003
Thomas Adams <adamst@emr.ac.uk>
2017-06-19 11:05:50 +0100

    Adds commands to create TP3 variable

--- BC-16_All_DEGs_venn_diag.r
+++ BC-16_All_DEGs_venn_diag.r
@@ -30,7 +30,7 @@ summary(df1)
 
 TP1=subset(df1, df1[,"24hr"] == 1 & df1[,"48hr"] == 0 & df1[,"96hr"] == 0)
 TP2=subset(df1, df1[,"24hr"] == 0 & df1[,"48hr"] == 1 & df1[,"96hr"] == 0)
-Others=subset(df1, df1[,"A4"] != 0 & df1[,"Nov5"] == 0 & df1[,"Nov27"] != 0 & df1[,"Nov71"] != 0 & df1[,"Bc16"] != 0 & df1[,"Nov9"] != 0 & df1[,"Bc1"] == 0)
+TP3=subset(df1, df1[,"24hr"] == 0 & df1[,"48hr"] == 0 & df1[,"96hr"] == 1)
 # orthologs=subset(df1, df1[,"A28"] == 1 & df1[,"CB3"] == 1 & df1[,"PG"] == 1 & df1[,"fo47"] == 1 & df1[,"A1_2"] == 1 & df1[,"Fus2"] == 1 & df1[,"125"] == 1 & df1[,"A23"] == 1 & df1[,"4287"] == 1)
 
 # area1=(nrow(nonpath) + nrow(orthologs))
