bee5f7f5b03e7699cc69e85f8dfe1bad407f2c45
Thomas Adams <adamst@emr.ac.uk>
2017-06-21 10:58:47 +0100

    Adds description of purpose of script

--- Functional_Annotation.py
+++ Functional_Annotation.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python
+
+'''
+This script parses functional annotations from interproscan and swissprot.
+Using gene lists of all genes and differentially expressed genes, it will output a table of functionally enriched annotations
+'''
