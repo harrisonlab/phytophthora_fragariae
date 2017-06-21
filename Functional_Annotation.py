bb270b5ee0ca2d7fae146a0adabc67a5778af44b
Thomas Adams <thomas.adams@emr.ac.uk>
2017-06-21 11:02:20 +0100

    Adds description on stage 1

--- Functional_Annotation.py
+++ Functional_Annotation.py
@@ -4,3 +4,8 @@
 This script parses functional annotations from interproscan and swissprot.
 Using gene lists of all genes and differentially expressed genes, it will output a table of functionally enriched annotations
 '''
+
+#-----------------------------------------------------
+# Stage 1
+# Import variables and load input files
+#-----------------------------------------------------
