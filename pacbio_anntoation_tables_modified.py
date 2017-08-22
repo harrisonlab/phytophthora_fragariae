11011b7a9398b3a352f2e1b30b121889f80898ed
Thomas Adams <thomas.adams@emr.ac.uk>
2017-08-22 15:46:10 +0100

    Adds commands to load RxLR_EER genes too

--- pacbio_anntoation_tables_modified.py
+++ pacbio_anntoation_tables_modified.py
@@ -284,7 +284,21 @@ for line in RxLR_total_lines:
     #     split_line = line.split()
     #     header = split_line[0].replace('>', '')
     #     RxLR_total_set.add(header)
-#
+
+#-----------------------------------------------------
+# Load RxLR_EER total +ve proteins into a set
+#-----------------------------------------------------
+
+RxLR_EER_total_set = Set()
+for line in RxLR_EER_total_lines:
+    header = line.rstrip()
+    RxLR_EER_total_set.add(header)
+    # line = line.rstrip()
+    # if line.startswith('>'):
+    #     split_line = line.split()
+    #     header = split_line[0].replace('>', '')
+    #     RxLR_total_set.add(header)
+
 # #-----------------------------------------------------
 # # Load CRN LFLAK hmm +ve proteins into a set
 # #-----------------------------------------------------
