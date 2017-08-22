4de212f37120c8fbd32d5509a3374f41da2a8a1c
Thomas Adams <adamst@emr.ac.uk>
2017-08-22 16:54:56 +0100

    Removes extra space

--- pacbio_anntoation_tables_modified.py
+++ pacbio_anntoation_tables_modified.py
@@ -584,7 +584,7 @@ for line in transcript_lines:
         trans_mem = 'Yes'
     # if old_id in gpi_set:
     #     gpi = 'Yes'
-    if any([sigP2 == 'Yes', sigP3 ==  'Yes', sigP4 == 'Yes']) and all([trans_mem == '']):
+    if any([sigP2 == 'Yes', sigP3 == 'Yes', sigP4 == 'Yes']) and all([trans_mem == '']):
         secreted = 'Yes'
     else:
         secreted = ''
