4b73a965b37d418b98dd2fc579f46cb7ea0abb82
Thomas Adams <thomas.adams@emr.ac.uk>
2017-08-23 11:01:52 +0100

    Adds commands to load RxLR & EER sets

--- pacbio_anntoation_tables_modified.py
+++ pacbio_anntoation_tables_modified.py
@@ -547,6 +547,8 @@ for line in transcript_lines:
     # gene_id = transcript_id.split('.')[0]
     if transcript_id in RxLR_total_set:
         RxLR_total = 'Yes'
+    if transcript_id in RxLR_EER_total_set:
+        RxLR_EER_total = 'Yes'
     # if transcript_id in CRN_LFLAK_set:
     #     CRN_LFLAK = 'Yes'
     # if transcript_id in CRN_DWL_set:
