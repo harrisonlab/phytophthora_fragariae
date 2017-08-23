4e75cd4655d53b60f25aa2788813083b316d8626
Thomas Adams <thomas.adams@emr.ac.uk>
2017-08-23 12:37:04 +0100

    Edits help text for phobius

--- pacbio_anntoation_tables_modified.py
+++ pacbio_anntoation_tables_modified.py
@@ -25,7 +25,7 @@ ap.add_argument('--gene_fasta',required=True,type=str,help='amino acid sequence 
 ap.add_argument('--SigP2',required=True,type=str,help='fasta file of genes testing positive for signal peptide using SigP2.0')
 ap.add_argument('--SigP3',required=True,type=str,help='fasta file of genes testing positive for signal peptide using SigP3.0')
 ap.add_argument('--SigP4',required=True,type=str,help='fasta file of genes testing positive for signal peptide using SigP4.1')
-ap.add_argument('--phobius',required=True,type=str,help='txt file of headers from gene testing positive for signal peptide using phobius')
+ap.add_argument('--phobius',required=True,type=str,help='fasta file of  gene testing positive for signal peptide using phobius')
 ap.add_argument('--trans_mem',required=True,type=str,help='txt file of headers from gene testing positive for tranmembrane proteins by TMHMM')
 # ap.add_argument('--GPI_anchor',required=True,type=str,help='txt file of headers from gene testing positive for GPI anchors as identified by GPI-SOM')
 #ap.add_argument('--RxLR_motif',required=True,type=str,help='fasta file of genes testing positive for RxLR-EER motifs')
