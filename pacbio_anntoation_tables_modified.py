991bd4401f21a5d9c2b543f1fb843ab8126744da
Thomas Adams <adamst@emr.ac.uk>
2017-08-23 10:35:05 +0100

    Adds argument for integrating SigP3.0 results

--- pacbio_anntoation_tables_modified.py
+++ pacbio_anntoation_tables_modified.py
@@ -23,6 +23,7 @@ ap.add_argument('--gff_format',required=True,type=str,choices=['gff3', 'gtf'],he
 ap.add_argument('--gene_gff',required=True,type=str,help='Gff file of predicyted gene models')
 ap.add_argument('--gene_fasta',required=True,type=str,help='amino acid sequence of predicted proteins')
 ap.add_argument('--SigP2',required=True,type=str,help='fasta file of genes testing positive for signal peptide using SigP2.0')
+ap.add_argument('--SigP3',required=True,type=str,help='fasta file of genes testing positive for signal peptide using SigP3.0')
 ap.add_argument('--SigP4',required=True,type=str,help='fasta file of genes testing positive for signal peptide using SigP4.1')
 ap.add_argument('--phobius',required=True,type=str,help='txt file of headers from gene testing positive for signal peptide using phobius')
 ap.add_argument('--trans_mem',required=True,type=str,help='txt file of headers from gene testing positive for tranmembrane proteins by TMHMM')
