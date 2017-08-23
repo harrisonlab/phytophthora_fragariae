2bb14ce226b6a0f49422c6d6c58e5c69314886a3
Thomas Adams <adamst@emr.ac.uk>
2017-08-23 11:05:45 +0100

    Comments out swissprot & interpro from outline commands

--- pacbio_anntoation_tables_modified.py
+++ pacbio_anntoation_tables_modified.py
@@ -613,7 +613,7 @@ for line in transcript_lines:
     outline.extend(mean_fpkm_cols)
     outline.extend(DEG_out)
     outline.append(prot_seq)
-    outline.extend(swissprot_cols)
-    outline.append(interpro_col)
+    # outline.extend(swissprot_cols)
+    # outline.append(interpro_col)
     print "\t".join(outline)
     # print DEG_out
