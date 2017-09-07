ed500d82fd052238823be49dc71cf0d1b0942fb8
Thomas Adams <adamst@emr.ac.uk>
2017-09-07 11:16:50 +0100

    Copies from Maria

---
+++ reserve_cluster.sh
@@ -0,0 +1,23 @@
+#$ -S /bin/bash
+#$ -l h_vmem=5G
+#$ -l mem_free=1G
+#$ -l virtual_free=1G
+#$ -pe smp 1
+#$ -l h_rt=9999:00:00
+
+# hold a node for a qlogin session
+
+#myqsub.sh ~/misc/sample_script.sh
+
+while true
+do
+    sleep 10
+done
+
+```
+The main resources you will be asking for are CPU(slots) and memory.
+
+The memory can be requested with h_vmem, this is the maximum amount of memory a slot can take, if it exceeds this amount your job will be automatically killed. Another memory related option is mem_free
+
+Now, we can also ask for more CPU using -pe option. This option stands for "parallel environment" and each cluster sets up a set of parallel environments for parallel processing. The syntax for the option is as follows:-pe [environment name] [number of slots].
+```
