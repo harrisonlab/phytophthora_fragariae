57840531fd70343b2557ed97a2bb6894d5b63e5d
Thomas Adams <thomas.adams@emr.ac.uk>
2018-01-16 14:08:51 +0000

    Adds commands to parse arguments

--- extract_DEG_Names.py
+++ extract_DEG_Names.py
@@ -6,3 +6,7 @@ This script uses the tsv file produced by parsing RNA-Seq with a previous python
 
 import sys,argparse
 from collections import defaultdict
+
+ap = argparse.ArgumentParser()
+ap.add_argument('--input',required=True,type=str,help='tsv file of binary presence table of DEGs')
+ap.add_argument('--output',reuqired=True,type=str,help='Output text file of gene names')
