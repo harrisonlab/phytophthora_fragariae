9bb77efc50bb60a7bdc3255789935d1eb0f00f08
Thomas Adams <adamst@emr.ac.uk>
2018-01-16 14:03:26 +0000

    Adds commands to import libraries

--- extract_DEG_Names.py
+++ extract_DEG_Names.py
@@ -3,3 +3,6 @@
 '''
 This script uses the tsv file produced by parsing RNA-Seq with a previous python script to extract only DEGs above a certain threshold
 '''
+
+import sys,argparse
+from collections import defaultdict
