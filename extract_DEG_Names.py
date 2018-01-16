5eab2317f3b26509de7e9c5081354f27a0084c41
Thomas Adams <adamst@emr.ac.uk>
2018-01-16 14:26:14 +0000

    Adds commands to load parsed tsv into dictionary

--- extract_DEG_Names.py
+++ extract_DEG_Names.py
@@ -10,3 +10,17 @@ from collections import defaultdict
 ap = argparse.ArgumentParser()
 ap.add_argument('--input',required=True,type=str,help='tsv file of binary presence table of DEGs')
 ap.add_argument('--output',required=True,type=str,help='Output text file of gene names')
+
+DEG_dict = defaultdict(list)
+with open(conf.input) as f:
+    DEG_lines = f.readlines()
+    for line in DEG_lines:
+        if line.startswith('Gene_Name'):
+            continue
+        else:
+            split_line = line.split()
+            gene_name = split_line[0]
+            time_a = split_line[1]
+            time_b = split_line[2]
+            time_c = split_line[3]
+            DEG_dict[gene_name].extend([time_a, time_b, time_c])
