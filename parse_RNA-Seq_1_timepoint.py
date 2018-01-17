2b5033a805f47e0a691006dc19f65a4bf1ac59bc
Thomas Adams <thomas.adams@emr.ac.uk>
2018-01-17 14:18:38 +0000

    Removes test commands for inputs 2 & 3

--- parse_RNA-Seq_1_timepoint.py
+++ parse_RNA-Seq_1_timepoint.py
@@ -56,28 +56,6 @@ for x in genes:
                 to_add.append('1')
             else:
                 to_add.append('0')
-    try:
-        c = inp2.index(x)
-    except ValueError:
-        to_add.append('0')
-    else:
-        for y in inp2_dict[x]:
-            test = abs(y)
-            if test > 1:
-                to_add.append('1')
-            else:
-                to_add.append('0')
-    try:
-        d = inp3.index(x)
-    except ValueError:
-        to_add.append('0')
-    else:
-        for y in inp3_dict[x]:
-            test = abs(y)
-            if test > 1:
-                to_add.append('1')
-            else:
-                to_add.append('0')
     a = numpy.append(a, to_add, axis=0)
 
 z = len(genes) + 1
