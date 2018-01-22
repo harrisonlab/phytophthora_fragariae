284ebf5c8b8cfa2e956cd52500e96875c417f1a9
Thomas Adams <thomas.adams@emr.ac.uk>
2018-01-22 16:53:32 +0000

    Adds commands to calculate median size of orthogroup

--- calc_orthogroup_median.py
+++ calc_orthogroup_median.py
@@ -2,3 +2,17 @@
 
 import sys,argparse
 import statistics
+
+ap = argparse.ArgumentParser()
+ap.add_argument('--orthogroups',required=True,type=str,help='orthoMCL output file')
+conf = ap.parse_args()
+
+sizes=[]
+with open(conf.orthogroups) as f:
+    ortho_lines = f.readlines()
+    for line in ortho_lines:
+        size=line.count('|')
+        sizes.append(size)
+
+median=statistics.median(sizes)
+print "median"
