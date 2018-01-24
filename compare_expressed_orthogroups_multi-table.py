fde7ac32647b28fcdaff8350b64e1d4c70ef99ef
Thomas Adams <thomas.adams@emr.ac.uk>
2018-01-24 16:28:46 +0000

    Adds title explaining script

--- compare_expressed_orthogroups_multi-table.py
+++ compare_expressed_orthogroups_multi-table.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python
+
+'''
+This script takes three gene tables produced by pacbio_anntoation_tables_modified.py as input and outputs text files with a list of uniquely differentially expressed genes, based on orthogroup.
+'''
