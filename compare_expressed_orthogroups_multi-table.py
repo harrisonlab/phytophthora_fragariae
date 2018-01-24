83dd23d8fecc8eeaa5eb1a57bf9735f33135b538
Thomas Adams <thomas.adams@emr.ac.uk>
2018-01-24 16:34:33 +0000

    Adds commands to import modules required

--- compare_expressed_orthogroups_multi-table.py
+++ compare_expressed_orthogroups_multi-table.py
@@ -3,3 +3,7 @@
 '''
 This script takes three gene tables produced by pacbio_anntoation_tables_modified.py as input and outputs text files with a list of uniquely differentially expressed genes, based on orthogroup.
 '''
+
+from sets import Set
+import sys,argparse
+from collections import defaultdict
