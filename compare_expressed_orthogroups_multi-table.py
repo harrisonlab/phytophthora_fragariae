29eb5a4df807960364912b5015251b0ce9e49857
Thomas Adams <thomas.adams@emr.ac.uk>
2018-01-25 16:34:00 +0000

    Adds commands to test for genes with LFC values only

--- compare_expressed_orthogroups_multi-table.py
+++ compare_expressed_orthogroups_multi-table.py
@@ -31,12 +31,18 @@ with open(conf.input_1) as f1:
         FPKM_48 = float(x.split('\t')[22])
         FPKM_96 = float(x.split('\t')[23])
         FPKM_Mycelium = float(x.split('\t')[24])
-        LFC_24 = float(x.split('\t')[25])
-        LFC_48 = float(x.split('\t')[27])
-        LFC_96 = float(x.split('\t')[29])
-        P_val_24 = float(x.split('\t')[26])
-        P_val_48 = float(x.split('\t')[28])
-        P_val_96 = float(x.split('\t')[30])
+        if x.split('\t')[25] != '.':
+            LFC_24 = float(x.split('\t')[25])
+        if x.split('\t')[27] != '.':
+            LFC_48 = float(x.split('\t')[27])
+        if x.split('\t')[29] != '.':
+            LFC_96 = float(x.split('\t')[29])
+        if x.split('\t')[26] != '.':
+            P_val_24 = float(x.split('\t')[26])
+        if x.split('\t')[28] != '.':
+            P_val_48 = float(x.split('\t')[28])
+        if x.split('\t')[30] != '.':
+            P_val_96 = float(x.split('\t')[30])
         if FPKM_24 >= 5 or FPKM_48 >= 5 or FPKM_96 >= 5 or FPKM_Mycelium >= 5:
             if (LFC_24 >= 1 and P_val_24 <= 0.05) or (LFC_48 >= 1 and P_val_48 <= 0.05) or (LFC_96 >= 1 and P_val_96 <=0.05):
                 gene_ID = x.split('\t')[0]
