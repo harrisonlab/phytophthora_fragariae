0d5893e55523f69f70bd49768970228c2cbbb483
Thomas Adams <thomas.adams@emr.ac.uk>
2018-01-25 14:58:08 +0000

    Adds commands for parsing arguments

--- compare_expressed_orthogroups_reference_based.py
+++ compare_expressed_orthogroups_reference_based.py
@@ -10,3 +10,12 @@ import sys,argparse
 # Step 1
 # Import variables, load input files and create lists of gene names
 #-----------------------------------------------------
+
+ap = argparse.ArgumentParser()
+ap.add_argument('--input_1',required=True,type=str,help='Gene table for reference isolate transcripts')
+ap.add_argument('--input_2',required=True,type=str,help='Gene table for transcripts of non-reference isolate #1')
+ap.add_argument('--input_3',required=True,type=str,help='Gene table for transcripts of non-reference isolate #2')
+ap.add_argument('--output_1',required=True,type=str,help='Text file for output of reference isolate candidates')
+ap.add_argument('--output_2',required=True,type=str,help='Text file for output of non-reference isolate #1')
+ap.add_argument('--output_3',required=True,type=str,help='Text file for output of non-reference isolate #2')
+conf = ap.parse_args()
