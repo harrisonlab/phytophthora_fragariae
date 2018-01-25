54b649095fd6165ff84ff05919f4a10d7f481240
Thomas Adams <thomas.adams@emr.ac.uk>
2018-01-25 14:45:03 +0000

    Adds command to load module

--- compare_expressed_orthogroups_reference_based.py
+++ compare_expressed_orthogroups_reference_based.py
@@ -3,3 +3,5 @@
 '''
 This script takes three gene tables produced by pacbio_anntoation_tables_modified.py as input and outputs text files with a list of uniquely differentially expressed genes, based on gene ID in BC-16.
 '''
+
+import sys,argparse
