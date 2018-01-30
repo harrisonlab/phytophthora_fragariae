c116f1b3c70e64bf1acceecb59377c7a5391db30
Thomas Adams <thomas.adams@emr.ac.uk>
2018-01-30 17:35:53 +0000

    Adds commands to specify organism_name variable

--- RNA_Seq_scripts/Extract_Expressed_Genes.py
+++ RNA_Seq_scripts/Extract_Expressed_Genes.py
@@ -12,6 +12,7 @@ import numpy as np
 ap = argparse.ArgumentParser()
 ap.add_argument('--FPKM_in',required=True,type=str,help='text output file from DeSeq2 commands of non-normalised FPKM values')
 ap.add_argument('--Orthogroup_in',required=True,type=str,help='text output file of Orthogroups from OrthoFinder')
+ap.add_argument('--Organism_name',required=True,type=str,help='Name of organism gene IDs are from in FPKM input file')
 ap.add_argument('--Output_1',required=True,type=str,help='Output text file for isolate 1, three timepoints')
 ap.add_argument('--Output_2',required=True,type=str,help='Output text file for isolate 2, one timepoint')
 ap.add_argument('--Output_3',required=True,type=str,help='Output text file for isolate 3, one timepoint')
