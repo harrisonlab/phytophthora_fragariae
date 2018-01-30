fb0b043b13ca03a42aa99accc7e123bf93d2f918
Thomas Adams <thomas.adams@emr.ac.uk>
2018-01-30 17:09:27 +0000

    Adds command to load orthogroups into list and edits help text of args

--- RNA_Seq_scripts/Extract_Expressed_Genes.py
+++ RNA_Seq_scripts/Extract_Expressed_Genes.py
@@ -12,15 +12,18 @@ import numpy as np
 ap = argparse.ArgumentParser()
 ap.add_argument('--FPKM_in',required=True,type=str,help='text output file from DeSeq2 commands of non-normalised FPKM values')
 ap.add_argument('--Orthogroup_in',required=True,type=str,help='text output file of Orthogroups from OrthoFinder')
-ap.add_argument('--Output_1',required=True,type=str,help='Output text file for isolate 1')
-ap.add_argument('--Output_2',required=True,type=str,help='Output text file for isolate 2')
-ap.add_argument('--Output_3',required=True,type=str,help='Output text file for isolate 3')
+ap.add_argument('--Output_1',required=True,type=str,help='Output text file for isolate 1, three timepoints')
+ap.add_argument('--Output_2',required=True,type=str,help='Output text file for isolate 2, one timepoint')
+ap.add_argument('--Output_3',required=True,type=str,help='Output text file for isolate 3, one timepoint')
 conf = ap.parse_args()
 
 #-----------------------------------------------------
 # Step 1
-# Load input files, create sets and dictionaries
+# Load input files
 #-----------------------------------------------------
 
 with open(conf.FPKM_in) as f:
     fpkm_lines=f.readlines()
+
+with open(conf.Orthogroup_in) as f:
+    Ortho_lines=f.readlines()
