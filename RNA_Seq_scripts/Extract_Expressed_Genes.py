b9bb7e91b88e924bb5af75a58cd4390695c275c7
Thomas Adams <thomas.adams@emr.ac.uk>
2018-01-30 16:42:44 +0000

    Adds commands to specify arguments for the script

--- RNA_Seq_scripts/Extract_Expressed_Genes.py
+++ RNA_Seq_scripts/Extract_Expressed_Genes.py
@@ -6,3 +6,11 @@ This script uses the output of DeSeq2 to produce a list of genes that are expres
 
 import sys,argparse
 from sets import Set
+
+ap = argparse.ArgumentParser()
+ap.add_argument('--FPKM_in',required=True,type=str,help='text output file from DeSeq2 commands of non-normalised FPKM values')
+ap.add_argument('--Orthogroup_in',required=True,type=str,help='text output file of Orthogroups from OrthoFinder')
+ap.add_argument('--Output_1',required=True,type=str,help='Output text file for isolate 1')
+ap.add_argument('--Output_2',required=True,type=str,help='Output text file for isolate 2')
+ap.add_argument('--Output_3',required=True,type=str,help='Output text file for isolate 3')
+conf = ap.parse_args()
