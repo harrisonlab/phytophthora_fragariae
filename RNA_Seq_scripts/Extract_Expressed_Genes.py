f07d6942ed8b58dccd8ce0842d14a1303d1c6a17
Thomas Adams <thomas.adams@emr.ac.uk>
2018-01-30 16:35:46 +0000

    Adds commands to import modules

--- RNA_Seq_scripts/Extract_Expressed_Genes.py
+++ RNA_Seq_scripts/Extract_Expressed_Genes.py
@@ -3,3 +3,6 @@
 '''
 This script uses the output of DeSeq2 to produce a list of genes that are expressed only in a single isolate and add orthogroup ID for each gene
 '''
+
+import sys,argparse
+from sets import Set
