4b07e912d781af3ca67f011778a7f0a46f7564f2
Thomas Adams <thomas.adams@emr.ac.uk>
2018-01-30 16:22:25 +0000

    Edits comment explaining purpose of script

--- RNA_Seq_scripts/Extract_Expressed_Genes.py
+++ RNA_Seq_scripts/Extract_Expressed_Genes.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
 
 '''
-This script uses the output of DeSeq2 to produce a list of genes that are expressed only in a single isolate
+This script uses the output of DeSeq2 to produce a list of genes that are expressed only in a single isolate and add orthogroup ID for each gene
 '''
