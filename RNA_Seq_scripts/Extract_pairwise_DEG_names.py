d43e60401db63d78784150fb8ff8f795c762ffb9
Thomas Adams <thomas.adams@emr.ac.uk>
2018-01-31 16:13:13 +0000

    Corrects reference name help text

--- RNA_Seq_scripts/Extract_pairwise_DEG_names.py
+++ RNA_Seq_scripts/Extract_pairwise_DEG_names.py
@@ -12,7 +12,7 @@ from sets import Set
 ap = argparse.ArgumentParser()
 ap.add_argument('--DEG_files',required=True,nargs='+',type=str,help='space spererated list of files containing DEG information')
 ap.add_argument('--Orthogroup_in',required=True,type=str,help='text output file of Orthogroups from OrthoFinder')
-ap.add_argument('--Reference_name',required=True,type=str,help='Name of organism gene IDs are from in FPKM input file')
+ap.add_argument('--Reference_name',required=True,type=str,help='Name of organism gene IDs are from in DEG input file')
 ap.add_argument('--Min_LFC',required=True,type=float,help='Minimum log fold change for a gene to be called a DEG')
 ap.add_argument('--Sig_Level',required=True,type=float,help='Minimum p-value for a DEG to be considered significant')
 conf = ap.parse_args()
