7a57920098e7210df0ce32e733d2e71ad3c77a5c
Thomas Adams <thomas.adams@emr.ac.uk>
2018-01-31 16:12:43 +0000

    Adds commands to load python modules

--- RNA_Seq_scripts/Extract_DEGs_names_vs_mycelium.py
+++ RNA_Seq_scripts/Extract_DEGs_names_vs_mycelium.py
@@ -3,3 +3,8 @@
 '''
 This script uses the output of DeSeq2 to produce a list of genes that are differntially expressed only in a single isolate and add orthogroup ID for each gene. For aligning multiple isolates RNA to one reference.
 '''
+
+import sys,argparse
+from collections import defaultdict
+import json
+from sets import Set
