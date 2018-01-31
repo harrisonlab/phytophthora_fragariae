92c7a245b99c67450cf41074c11fc9db5d906451
Thomas Adams <thomas.adams@emr.ac.uk>
2018-01-31 11:53:22 +0000

    Edits commands to allow specification of LFC and Pval cutoffs

--- RNA_Seq_scripts/Extract_pairwise_DEG_names.py
+++ RNA_Seq_scripts/Extract_pairwise_DEG_names.py
@@ -14,6 +14,8 @@ ap = argparse.ArgumentParser()
 ap.add_argument('--DEG_files',required=True,nargs='+',type=str,help='space spererated list of files containing DEG information')
 ap.add_argument('--Orthogroup_in',required=True,type=str,help='text output file of Orthogroups from OrthoFinder')
 ap.add_argument('--Organism_name',required=True,type=str,help='Name of organism gene IDs are from in FPKM input file')
+ap.add_argument('--Min_LFC',required=True,type=float,help='Minimum log fold change for a gene to be called a DEG')
+ap.add_argument('--Sig_Level',required=True,type=float,help='Minimum p-value for a DEG to be considered significant')
 ap.add_argument('--Output_1',required=True,type=str,help='Output text file for isolate 1, three timepoints')
 ap.add_argument('--Output_2',required=True,type=str,help='Output text file for isolate 2, one timepoint')
 ap.add_argument('--Output_3',required=True,type=str,help='Output text file for isolate 3, one timepoint')
@@ -23,6 +25,9 @@ conf = ap.parse_args()
 # Step 1
 # Load input files
 #-----------------------------------------------------
+
+LFC = conf.Min_LFC
+Sig_Level = conf.Sig_Level
 
 DEG_files = conf.DEG_files
 DEG_dict = defaultdict(list)
@@ -38,7 +43,7 @@ for DEG_file in DEG_files:
                 gene_name = split_line[0]
                 log_change = split_line[2]
                 P_val = split_line[6]
-                if log_change >= 1 and P_val <= 0.05:
+                if abs(log_change) >= LFC and P_val <= Sig_Level:
                     entryname = "_".join([filename, gene_name])
                     DEG_dict[entryname].extend([log_change, P_val])
 
