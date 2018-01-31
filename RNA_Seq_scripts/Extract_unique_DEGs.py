16fe468da726a36cb29480559b9d75a3e380ba2c
Thomas Adams <thomas.adams@emr.ac.uk>
2018-01-31 11:05:01 +0000

    Adds comment explaining script purpose

--- RNA_Seq_scripts/Extract_unique_DEGs.py
+++ RNA_Seq_scripts/Extract_unique_DEGs.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python
+
+'''
+This script uses the output of DeSeq2 to produce a list of genes that are differntially expressed only in a single isolate and add orthogroup ID for each gene
+'''
