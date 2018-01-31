36df3e58200d769aba154550fec8a7e2d26333c4
Thomas Adams <thomas.adams@emr.ac.uk>
2018-01-31 16:13:33 +0000

    Adds commands to specify arguments

--- RNA_Seq_scripts/Extract_DEGs_names_vs_mycelium.py
+++ RNA_Seq_scripts/Extract_DEGs_names_vs_mycelium.py
@@ -8,3 +8,11 @@ import sys,argparse
 from collections import defaultdict
 import json
 from sets import Set
+
+ap = argparse.ArgumentParser()
+ap.add_argument('--DEG_files',required=True,nargs='+',type=str,help='space spererated list of files containing DEG information')
+ap.add_argument('--Orthogroup_in',required=True,type=str,help='text output file of Orthogroups from OrthoFinder')
+ap.add_argument('--Reference_name',required=True,type=str,help='Name of organism gene IDs are from in DEG input file')
+ap.add_argument('--Min_LFC',required=True,type=float,help='Minimum log fold change for a gene to be called a DEG')
+ap.add_argument('--Sig_Level',required=True,type=float,help='Minimum p-value for a DEG to be considered significant')
+conf = ap.parse_args()
