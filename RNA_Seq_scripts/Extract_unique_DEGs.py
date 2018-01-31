5960416bb7237814590416d13143a088261d000e
Thomas Adams <thomas.adams@emr.ac.uk>
2018-01-31 11:43:11 +0000

    Adds commands to load DEGs & orthology groups

--- RNA_Seq_scripts/Extract_unique_DEGs.py
+++ RNA_Seq_scripts/Extract_unique_DEGs.py
@@ -23,3 +23,32 @@ conf = ap.parse_args()
 # Step 1
 # Load input files
 #-----------------------------------------------------
+
+DEG_files = conf.DEG_files
+DEG_dict = defaultdict(list)
+for DEG_file in DEG_files:
+    with open(DEG_file) as f:
+        filename = DEG_file
+        DEG_lines = f.readlines()
+        for line in DEG_lines:
+            if line.startswith('baseMean'):
+                continue
+            else:
+                split_line = line.split()
+                gene_name = split_line[0]
+                log_change = split_line[2]
+                P_val = split_line[6]
+                entryname = "_".join([filename, gene_name])
+                DEG_dict[entryname].extend([log_change, P_val])
+
+organism_name = conf.Organism_name
+ortho_dict = defaultdict(list)
+for line in Ortho_lines:
+    line = line.rstrip()
+    split_line = line.split()
+    orthogroup = split_line[0]
+    orthogroup = orthogroup.replace(":", "")
+    genes_in_group = [ x for x in split_line if organism_name in x ]
+    for gene in genes_in_group:
+        gene = gene.replace(organism_name, '').replace('|', '')
+        ortho_dict[gene] = orthogroup
