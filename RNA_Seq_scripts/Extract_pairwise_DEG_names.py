e544e2de103c4fcfafbd9a5804591967bf391af6
Thomas Adams <thomas.adams@emr.ac.uk>
2018-01-31 14:52:12 +0000

    Adds commands for organism 3 vs organism 1

--- RNA_Seq_scripts/Extract_pairwise_DEG_names.py
+++ RNA_Seq_scripts/Extract_pairwise_DEG_names.py
@@ -145,3 +145,20 @@ Org2_vs_Org3_dict = defaultdict(str)
 for transcript in Org2_vs_Org3_set:
     orthogroup = ortho_dict[transcript]
     Org2_vs_Org3_dict[transcript].extend(orthogroup)
+
+#Organism3 vs Organism1
+Org3_vs_Org1 = []
+for item in DEG_list:
+    if item.split('/')[-1].split('_')[0] == Organism3 and item.split('/')[-1].split('_')[5] == "up":
+        transcript_id = item.split('/').split('_')[6]
+        Org3_vs_Org1.append(transcript_id)
+    elif item.split('/')[-1].split('_')[3] == Organism3 and item.split('/')[-1].split('_')[5] == "down":
+        transcript_id = item.split('/').split('_')[6]
+        Org3_vs_Org1.append(transcript_id)
+
+Org3_vs_Org1_set = set(Org3_vs_Org1)
+Org3_vs_Org1_dict = defaultdict(str)
+
+for transcript in Org3_vs_Org1_set:
+    orthogroup = ortho_dict[transcript]
+    Org3_vs_Org1_dict[transcript].extend(orthogroup)
