0f3fddee8c672c0c57856e6cae1391ac95e3ce08
Thomas Adams <thomas.adams@emr.ac.uk>
2018-02-01 14:23:42 +0000

    Edits statement for introduction

--- RNA_Seq_scripts/Identify_unique_Expressed_Genes.py
+++ RNA_Seq_scripts/Identify_unique_Expressed_Genes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 '''
-This script uses the output of DeSeq2 to produce a list of genes that are expressed only in a single isolate and add orthogroup ID for each gene
+This script uses the output of DeSeq2 to produce a list of genes that are expressed only in a single isolate and add orthogroup ID for each gene, also identifies and classifies unique genes
 '''
 
 import sys,argparse
