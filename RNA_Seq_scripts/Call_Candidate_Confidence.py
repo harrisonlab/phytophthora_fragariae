00cb47e64f030eab820e491401ca1a7a2fbcbc61
Thomas Adams <thomas.adams@emr.ac.uk>
2018-02-06 09:26:37 +0000

    Adds commands for loading libraries

--- RNA_Seq_scripts/Call_Candidate_Confidence.py
+++ RNA_Seq_scripts/Call_Candidate_Confidence.py
@@ -3,3 +3,8 @@
 '''
 This script takes outputs from Expression presence/absence and differently DEGs to create a sorted table of candidates with a score of 1-9, 9 being best
 '''
+
+import sys,argparse
+from collections import defaultdict
+from sets import Set
+import os
