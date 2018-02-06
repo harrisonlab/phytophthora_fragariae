1c1aef2c85d6e860e4452abb936023453a906f7b
Thomas Adams <thomas.adams@emr.ac.uk>
2018-02-06 10:07:30 +0000

    Adds commands to load arguments

--- RNA_Seq_scripts/Call_Candidate_Confidence.py
+++ RNA_Seq_scripts/Call_Candidate_Confidence.py
@@ -10,3 +10,15 @@ from sets import Set
 import os
 
 ap = argparse.ArgumentParser()
+ap.add_argument('--Unique_Expression_Files',required=True,nargs='+',type=str,help='Space separated list of files containing uniquely expressed genes')
+ap.add_argument('--Differently_DEG_File',required=True,nargs='+',type=str,help='Space separated list of files containing unique differently differentially expressed genes')
+ap.add_argument('--Organism_1',required=True,type=str,help='ID of organism 1')
+ap.add_argument('--Organism_2',required=True,type=str,help='ID of organism 2')
+ap.add_argument('--Organism_3',required=True,type=str,help='ID of organism 3')
+ap.add_argument('--Reference_name',required=True,type=str,help='ID of isolate to score candidates of')
+ap.add_argument('--RxLRs',required=True,type=str,help='File of all RxLR names for aligned genome')
+ap.add_argument('--CRNs',required=True,type=str,help='File of all CRN names for aligned genome')
+ap.add_argument('--ApoP',required=True,type=str,help='File of all hits from ApoplastP')
+ap.add_argument('--Secreted_CQ',required=True,type=str,help='File of all secreted gene models')
+ap.add_argument('--Secreted_ORF',required=True,type=str,help='File of all secreted ORF fragments')
+ap.add_argument('--OutDir',required=True,type=str,help='Directory to write output files to')
