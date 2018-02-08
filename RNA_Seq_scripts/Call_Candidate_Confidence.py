87dda71f5cde6dd2e68195765afd9939ccc8b584
Thomas Adams <thomas.adams@emr.ac.uk>
2018-02-08 16:33:51 +0000

    Edits comment explaining script purpose

--- RNA_Seq_scripts/Call_Candidate_Confidence.py
+++ RNA_Seq_scripts/Call_Candidate_Confidence.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 '''
-This script takes outputs from Expression presence/absence and differently DEGs to create a sorted table of candidates with a score of 1-9, 9 being best
+This script takes outputs from Expression presence/absence and differently DEGs to create a sorted table of candidates with a score of 0-6, 6 being best
 '''
 
 import sys,argparse
