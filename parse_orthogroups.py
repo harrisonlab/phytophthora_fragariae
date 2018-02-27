90109452ab6e015a3904ea110690b3e3fe34929a
Thomas Adams <thomas.adams@emr.ac.uk>
2018-02-27 12:21:28 +0000

    Makes changes to script to conform to pep8

--- parse_orthogroups.py
+++ parse_orthogroups.py
@@ -1,35 +1,36 @@
 #!/usr/bin/python
 
 '''
-This script uses an orthology group txt file output from OrthoMCL to create a count table for the number of genes per strain in each orthogroup
+This script uses an orthology group txt file output from OrthoMCL to create a
+count table for the number of genes per strain in each orthogroup
 '''
 
-from sets import Set
-import sys,argparse
+import argparse
 from collections import defaultdict
-import re
 import numpy
 import csv
 
-#-----------------------------------------------------
+# -----------------------------------------------------
 # Step 1
 # Import variables & load input files
 # Copied from orthoMCLgroups2fasta.py
-#-----------------------------------------------------
+# -----------------------------------------------------
 
 ap = argparse.ArgumentParser()
-ap.add_argument('--orthogroups',required=True,type=str,help='text file output of OrthoMCL orthogroups')
-ap.add_argument('--out_dir',required=True,type=str,help='the tsv file where the count table is output to')
+ap.add_argument('--orthogroups', required=True, type=str, help='text file output \
+                of OrthoMCL orthogroups')
+ap.add_argument('--out_dir', required=True, type=str, help='the tsv file where \
+                the count table is output to')
 conf = ap.parse_args()
 
 with open(conf.orthogroups) as f:
     ortho_lines = f.readlines()
 
-#-----------------------------------------------------
+# -----------------------------------------------------
 # Step 2
 # Build a dictionary of orthogroups
 # Copied from orthoMCLgroups2fasta.py
-#-----------------------------------------------------
+# -----------------------------------------------------
 
 ortho_dict = defaultdict(list)
 for line in ortho_lines:
@@ -40,16 +41,19 @@ for line in ortho_lines:
     for gene in split_line[1:]:
         ortho_dict[orthogroup].append(gene)
 
-#-----------------------------------------------------
+# -----------------------------------------------------
 # Step 3
 # Create counts for each entry and write to a table
-#-----------------------------------------------------
-# For each strain in each orthogroup, add the constituent genes to a set. Count the number of genes in this set and print to a table. Strain names and reshape commands need to be specified any time this script is run.
+# -----------------------------------------------------
+# For each strain in each orthogroup, add the constituent genes to a set.
+# Count the number of genes in this set and print to a table.
+# Strain names and reshape commands need to be specified every time.
 
 keys = []
 sorted_keys = []
 keys = ortho_dict.keys()
-a = numpy.array(["Orthogroup", "A4", "Bc1", "Bc16", "Bc23", "Nov27", "Nov5", "Nov71", "Nov77", "Nov9", "ONT3", "SCRP245_v2"], dtype='a10')
+a = numpy.array(["Orthogroup", "A4", "Bc1", "Bc16", "Bc23", "Nov27", "Nov5",
+                 "Nov71", "Nov77", "Nov9", "ONT3", "SCRP245_v2"], dtype='a10')
 
 keys.sort()
 ortho_list = []
@@ -68,7 +72,8 @@ for group_name in keys:
     ONT3 = str(ortho_dict[group_name]).count("ONT3|")
     SCRP245 = str(ortho_dict[group_name]).count("SCRP245_v2|")
     group_names = group_name
-    to_add = [group_names, A4, Bc1, Bc16, Bc23, Nov27, Nov5, Nov71, Nov77, Nov9, ONT3, SCRP245]
+    to_add = [group_names, A4, Bc1, Bc16, Bc23, Nov27, Nov5, Nov71, Nov77,
+              Nov9, ONT3, SCRP245]
     a = numpy.append(a, to_add, axis=0)
 
 z = len(keys) + 1
