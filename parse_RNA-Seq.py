c53f33ccf19cced127c5e0158df92596246157c9
Thomas Adams <thomas.adams@emr.ac.uk>
2018-02-27 12:27:48 +0000

    Modifies script to conform to pep8

--- parse_RNA-Seq.py
+++ parse_RNA-Seq.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python
 
 '''
-This script uses text files of upregulated genes to create a count table for which genes are upregulated at which timepoint
+This script uses text files of upregulated genes to create a count table for
+which genes are upregulated at which timepoint
 '''
 
-from sets import Set
-import sys,argparse
+import argparse
 from collections import defaultdict
-import re
 import numpy
 import csv
 
-#-----------------------------------------------------
+# -----------------------------------------------------
 # Step 1
 # Import variables, load input files & create set of genes
-# If using a different number of files, arguments & appending to list of genes will need to be changed
-#-----------------------------------------------------
+# If using a different number of files, arguments & appending to list of genes
+# will need to be changed
+# -----------------------------------------------------
 
 ap = argparse.ArgumentParser()
-ap.add_argument('--input_1',required=True,type=str,help='text file of genes at 24hrs')
-ap.add_argument('--input_2',required=True,type=str,help='text file of genes at 48hrs')
-ap.add_argument('--input_3',required=True,type=str,help='text file of genes at 96hrs')
-ap.add_argument('--out_dir',required=True,type=str,help='the tsv file where the count table is output to')
+ap.add_argument('--input_1', required=True, type=str, help='text file of genes \
+at 24hrs')
+ap.add_argument('--input_2', required=True, type=str, help='text file of genes \
+at 48hrs')
+ap.add_argument('--input_3', required=True, type=str, help='text file of genes \
+at 96hrs')
+ap.add_argument('--out_dir', required=True, type=str, help='the tsv file where \
+the count table is output to')
 conf = ap.parse_args()
 
 inp1_dict = defaultdict(list)
@@ -60,11 +64,12 @@ with open(conf.input_3) as f3:
 
 genes = set(genes_list)
 
-#-----------------------------------------------------
+# -----------------------------------------------------
 # Step 2
 # Load gene names to a numpy array and create new columns
-# If doing with a different number of files, change the number in the numpy.reshape() command
-#-----------------------------------------------------
+# If doing with a different number of files
+# change the number in the numpy.reshape() command
+# -----------------------------------------------------
 
 a = numpy.array(["Gene_Name", "24hr", "48hr", "96hr"])
 
