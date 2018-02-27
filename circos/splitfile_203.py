7fda6555e4fa7beba68217b91ad0157b2596be99
Thomas Adams <thomas.adams@emr.ac.uk>
2018-02-27 12:28:48 +0000

    Modifies script to conform to pep8

--- circos/splitfile_203.py
+++ circos/splitfile_203.py
@@ -5,26 +5,29 @@ This tool splits a fasta file with many accessions down into a number of
 smaller files, each contatining 203 accessions.
 '''
 
-import sys,argparse
+import argparse
 from Bio import SeqIO
 
 ap = argparse.ArgumentParser()
-ap.add_argument('--inp_fasta',required=True,type=str,help='input FASTA file')
-ap.add_argument('--out_dir',required=True,type=str,help='the directory for split files to be written to')
-ap.add_argument('--out_base',required=True,type=str,help='the basename for output files')
+ap.add_argument('--inp_fasta', required=True, type=str, help='input FASTA \
+file')
+ap.add_argument('--out_dir', required=True, type=str, help='the directory for \
+split files to be written to')
+ap.add_argument('--out_base', required=True, type=str, help='the basename for \
+output files')
 conf = ap.parse_args()
 
 f = open(conf.inp_fasta)
 out_dir = conf.out_dir
 n = conf.out_base
 
-fasta_sequences = SeqIO.parse(f,'fasta')
+fasta_sequences = SeqIO.parse(f, 'fasta')
 
 
 i = 0
 file_num = 203
 new_file = out_dir + "/" + n + "_" + str(file_num) + ".fa"
-cur_file=open(new_file, 'w')
+cur_file = open(new_file, 'w')
 for seq_record in fasta_sequences:
     i += 1
     if i <= 203:
