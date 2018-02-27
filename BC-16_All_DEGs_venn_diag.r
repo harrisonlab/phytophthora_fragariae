f3329a02a94420cd18414c1cda6f2c47c93ca597
Thomas Adams <thomas.adams@emr.ac.uk>
2018-02-27 16:49:19 +0000

    Modifies code to community standards

--- BC-16_All_DEGs_venn_diag.r
+++ BC-16_All_DEGs_venn_diag.r
@@ -7,61 +7,53 @@
 
 # The script also requires the colorspace package. This can be downloaded by
 # opening R and running the following command:
-# options(download.file.method = "wget")
-# install.packages("colorspace")
 
 #get config options
-library(optparse)
-library(colorspace)
-library(VennDiagram, lib.loc="/home/armita/R-packages/")
-opt_list = list(
-    make_option("--inp", type="character", help="tab seperated file containing matrix of DEGs"),
-    make_option("--out", type="character", help="output venn diagram in pdf format")
+library("optparse")
+library("colorspace")
+library("VennDiagram")
+opt_list <- list(
+    make_option("--inp", type = "character",
+    help = "tab seperated file containing matrix of DEGs"),
+    make_option("--out", type = "character",
+    help = "output venn diagram in pdf format")
 )
-opt = parse_args(OptionParser(option_list=opt_list))
-f = opt$inp
-o = opt$out
+opt <- parse_args(OptionParser(option_list = opt_list))
+f <- opt$inp
+o <- opt$out
 
-DEGs <-data.frame()
+DEGs <- data.frame()
 DEGs <- read.table(f, header = TRUE)
 
-TP1=subset(DEGs, DEGs[,"X24hr"] == 1 & DEGs[,"X48hr"] == 0 & DEGs[,"X96hr"] == 0)
-TP2=subset(DEGs, DEGs[,"X24hr"] == 0 & DEGs[,"X48hr"] == 1 & DEGs[,"X96hr"] == 0)
-TP3=subset(DEGs, DEGs[,"X24hr"] == 0 & DEGs[,"X48hr"] == 0 & DEGs[,"X96hr"] == 1)
-# orthologs=subset(df1, df1[,"A28"] == 1 & df1[,"CB3"] == 1 & df1[,"PG"] == 1 & df1[,"fo47"] == 1 & df1[,"A1_2"] == 1 & df1[,"Fus2"] == 1 & df1[,"125"] == 1 & df1[,"A23"] == 1 & df1[,"4287"] == 1)
-
-# area1=(nrow(nonpath) + nrow(orthologs))
-# area2=(nrow(path) + nrow(orthologs))
-# area3=(nrow(tomato) + nrow(orthologs))
-# area3
-# area2
-# area1
-
-
-
-# Print labels
-# label1 <- paste('Path', ' (', area2, ')', sep="" )
-# label2 <- paste('NonPath', ' (', area1, ')', sep="" )
-# label3 <- paste('FoL', ' (', area3, ')', sep="" )
+TP1 <- subset(DEGs, DEGs[, "X24hr"] == 1 &
+DEGs[, "X48hr"] == 0 & DEGs[, "X96hr"] == 0)
+TP2 <- subset(DEGs, DEGs[, "X24hr"] == 0 &
+DEGs[, "X48hr"] == 1 & DEGs[, "X96hr"] == 0)
+TP3 <- subset(DEGs, DEGs[, "X24hr"] == 0 &
+DEGs[, "X48hr"] == 0 & DEGs[, "X96hr"] == 1)
 
 # Set up labels
-label1 <- paste("24hr", sep="" )
-label2 <- paste("48hr", sep="" )
-label3 <- paste("96hr", sep="" )
+label1 <- paste("24hr", sep = "" )
+label2 <- paste("48hr", sep = "" )
+label3 <- paste("96hr", sep = "" )
 
-n123=nrow(subset(DEGs, DEGs[,"X24hr"] == 1 & DEGs[,"X48hr"] == 1 & DEGs[,"X96hr"] == 1))
-n12=n123 + nrow(subset(DEGs, DEGs[,"X24hr"] == 1 & DEGs[,"X48hr"] == 1 & DEGs[,"X96hr"] == 0))
-n13=n123 + nrow(subset(DEGs, DEGs[,"X24hr"] == 1 & DEGs[,"X48hr"] == 0 & DEGs[,"X96hr"] == 1))
-n23=n123 + nrow(subset(DEGs, DEGs[,"X24hr"] == 0 & DEGs[,"X48hr"] == 1 & DEGs[,"X96hr"] == 1))
+n123 <- nrow(subset(DEGs, DEGs[, "X24hr"] == 1 &
+DEGs[, "X48hr"] == 1 & DEGs[, "X96hr"] == 1))
+n12 <- n123 + nrow(subset(DEGs, DEGs[, "X24hr"] == 1 &
+DEGs[, "X48hr"] == 1 & DEGs[, "X96hr"] == 0))
+n13 <- n123 + nrow(subset(DEGs, DEGs[, "X24hr"] == 1 &
+DEGs[, "X48hr"] == 0 & DEGs[, "X96hr"] == 1))
+n23 <- n123 + nrow(subset(DEGs, DEGs[, "X24hr"] == 0 &
+DEGs[, "X48hr"] == 1 & DEGs[, "X96hr"] == 1))
 summary(n12)
 summary(n13)
 summary(n23)
 summary(n123)
 
-area1=(nrow(TP1) + (n12 - n123) + (n13 - n123) + n123)
-area2=(nrow(TP2) + (n12 - n123) + (n23 - n123) + n123)
-area3=(nrow(TP3) + (n13 - n123) + (n23 - n123) + n123)
-#nrow(nonpath)
+area1 <- (nrow(TP1) + (n12 - n123) + (n13 - n123) + n123)
+area2 <- (nrow(TP2) + (n12 - n123) + (n23 - n123) + n123)
+area3 <- (nrow(TP3) + (n13 - n123) + (n23 - n123) + n123)
+
 nrow(TP1)
 nrow(TP2)
 nrow(TP3)
@@ -70,7 +62,6 @@ n13
 n23
 n123
 area1
-#area1 - n12 - n13 + n123
 area2
 area3
 
