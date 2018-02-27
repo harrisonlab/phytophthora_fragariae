65d3ef1f96490d9e29b3e331ee5cf37ef033d2c7
Thomas Adams <thomas.adams@emr.ac.uk>
2018-02-27 12:32:05 +0000

    Modifies script to conform to pep8

--- popgen_analysis/UK2_polarisation.py
+++ popgen_analysis/UK2_polarisation.py
@@ -1,35 +1,35 @@
-#Import modues
+# Import modues
 
 import numpy
 import csv
-import glob
 
-#Import dataset to numpy
+# Import dataset to numpy
 
-Inital_tbl = numpy.loadtxt(fname="Parsed_Polarising_95m_contigs_unmasked.tbl", dtype=object)
+Inital_tbl = numpy.loadtxt(fname="Parsed_Polarising_95m_contigs_unmasked.tbl",
+                           dtype=object)
 
-#Cut down table to give equal variants
+# Cut down table to give equal variants
 
-tbl2 = Inital_tbl[Inital_tbl[:,6] == Inital_tbl[:,4]]
-tbl3 = tbl2[tbl2[:,6] == tbl2[:,11]]
-tbl4 = tbl3[tbl3[:,6] == tbl3[:,12]]
-tbl5 = tbl4[tbl4[:,6] == tbl4[:,13]]
+tbl2 = Inital_tbl[Inital_tbl[:, 6] == Inital_tbl[:, 4]]
+tbl3 = tbl2[tbl2[:, 6] == tbl2[:, 11]]
+tbl4 = tbl3[tbl3[:, 6] == tbl3[:, 12]]
+tbl5 = tbl4[tbl4[:, 6] == tbl4[:, 13]]
 
-#Further cut down for different variants
+# Further cut down for different variants
 
-tbl6 = tbl5[tbl5[:,6] != tbl5[:,5]]
-tbl7 = tbl6[tbl6[:,6] != tbl6[:,7]]
-tbl8 = tbl7[tbl7[:,6] != tbl7[:,8]]
-tbl9 = tbl8[tbl8[:,6] != tbl8[:,9]]
-tbl10 = tbl9[tbl9[:,6] != tbl9[:,10]]
+tbl6 = tbl5[tbl5[:, 6] != tbl5[:, 5]]
+tbl7 = tbl6[tbl6[:, 6] != tbl6[:, 7]]
+tbl8 = tbl7[tbl7[:, 6] != tbl7[:, 8]]
+tbl9 = tbl8[tbl8[:, 6] != tbl8[:, 9]]
+tbl10 = tbl9[tbl9[:, 6] != tbl9[:, 10]]
 
-#Create and append Header row
+# Create and append Header row
 
-header = Inital_tbl[0,:]
+header = Inital_tbl[0, :]
 CSV = numpy.append(header, tbl10)
 CSV = numpy.reshape(CSV, (2, 14))
 
-#Print resulting table to a .csv
+# Print resulting table to a .csv
 
 with open("UK2_polarisation.csv", "wb") as f:
     writer = csv.writer(f)
