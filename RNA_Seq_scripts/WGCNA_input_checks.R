d8b21ddae19c05a8e902e836bc16a1dad324d49d
Thomas Adams <thomas.adams@emr.ac.uk>
2018-03-07 17:24:06 +0000

    Adds commands to load RNA-Seq data

--- RNA_Seq_scripts/WGCNA_input_checks.R
+++ RNA_Seq_scripts/WGCNA_input_checks.R
@@ -1,5 +1,16 @@
 # Load WGCNA package
 
-library(WGCNA)
+library("WGCNA")
+library("optparse")
 
 options(stringsAsFactors = FALSE)
+
+opt_list <- list(
+  make_option("--gene_table", type = "character",
+  help = "Input file of RNA-Seq data")
+  )
+
+opt <- parse_args(OptionParser(option_list = opt_list))
+inp <- opt$gene_table
+
+Exp_Data <- read.table(inp, header = FALSE, sep = "\t", stringsAsFactors = TRUE)
