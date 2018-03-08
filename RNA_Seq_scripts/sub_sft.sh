b6bde90f91d0828886f3929ba6fc2b80c124334d
Thomas Adams <thomas.adams@emr.ac.uk>
2018-03-08 16:55:05 +0000

    Adds commands to run choosing sft R script on worker nodes

---
+++ RNA_Seq_scripts/sub_sft.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -cwd
+#$ -pe smp 4
+#$ -l h_vmem=8G
+#$ -l h=blacklace01.blacklace|blacklace02.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace|blacklace12.blacklace
+
+scripts=/home/adamst/git_repos/scripts/phytophthora_fragariae/RNA_Seq_scripts
+
+/home/adamst/prog/R/R-3.2.5/bin/Rscript --vanilla $scripts/choosesoftthreshold.R
