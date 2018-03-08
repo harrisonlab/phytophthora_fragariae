713fa993dfff0231450879510f66c19d58fabb1a
Thomas Adams <thomas.adams@emr.ac.uk>
2018-03-08 11:23:51 +0000

    Adds appropriate comments

--- RNA_Seq_scripts/WGCNA_input_checks.R
+++ RNA_Seq_scripts/WGCNA_input_checks.R
@@ -3,7 +3,11 @@
 library("WGCNA")
 library("optparse")
 
+# Important option recommended by WGCNA
+
 options(stringsAsFactors = FALSE)
+
+# Parse in arguments to script
 
 opt_list <- list(
   make_option("--gene_table", type = "character",
@@ -13,7 +17,11 @@ opt_list <- list(
 opt <- parse_args(OptionParser(option_list = opt_list))
 inp <- opt$gene_table
 
+# Load input file
+
 exp_data <- read.tsv(inp, sep = "\t")
+
+# Parse data as WGCNA tutorial recommends
 
 datexpr0 <- as.data.frame(t(exp_data[, c(26:33)]))
 names(datexpr0) <- exp_data$transcript_id
