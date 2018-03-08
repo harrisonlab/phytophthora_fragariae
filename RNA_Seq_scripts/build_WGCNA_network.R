bb20f142ca858c7a15339c402d94946d20c9b4e2
Thomas Adams <thomas.adams@emr.ac.uk>
2018-03-08 14:59:01 +0000

    Adds commands to parse arguments

--- RNA_Seq_scripts/build_WGCNA_network.R
+++ RNA_Seq_scripts/build_WGCNA_network.R
@@ -12,3 +12,13 @@ options(stringsAsFactors = FALSE)
 # Allow multi-threading
 
 enableWGCNAThreads()
+
+# Parse arguments
+
+opt_list <- list(
+make_option("--out_dir", type = "character",
+  help = "Directory for plots to be written to")
+  )
+
+opt <- parse_args(OptionParser(option_list = opt_list))
+outdir <- opt$out_dir
