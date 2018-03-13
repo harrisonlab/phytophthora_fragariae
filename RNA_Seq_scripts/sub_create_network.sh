c1d6026953583a566df7e051c70c31ec0483e31f
Thomas Adams <thomas.adams@emr.ac.uk>
2018-03-13 17:29:19 +0000

    adds commands to export variable to allow multithreading

--- RNA_Seq_scripts/sub_create_network.sh
+++ RNA_Seq_scripts/sub_create_network.sh
@@ -6,3 +6,5 @@
 #$ -l h=blacklace01.blacklace|blacklace02.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace|blacklace12.blacklace
 
 scripts=/home/adamst/git_repos/scripts/phytophthora_fragariae/RNA_Seq_scripts
+
+export ALLOW_WGCA_THREADS=4
