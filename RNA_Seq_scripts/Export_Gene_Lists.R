2bc3b82742e0e8e08cd414c26a2349d05f31b7a2
Thomas Adams <thomas.adams@emr.ac.uk>
2018-03-15 10:18:36 +0000

    Adds commands to load arguments

--- RNA_Seq_scripts/Export_Gene_Lists.R
+++ RNA_Seq_scripts/Export_Gene_Lists.R
@@ -8,3 +8,13 @@ library("optparse")
 # Import option recommended in WGCNA documentation
 
 options(stringsAsFactors = FALSE)
+
+# Parse arguments
+
+opt_list <- list(
+    make_option("--out_dir", type = "character",
+    help = "Directory for output to be written to")
+    )
+
+opt <- parse_args(OptionParser(option_list = opt_list))
+outdir <- opt$out_dir
