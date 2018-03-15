cfddbc971ddbaa7ddabcc400010d54c088d004a1
Thomas Adams <thomas.adams@emr.ac.uk>
2018-03-15 17:02:23 +0000

    Adds commands to run R script for exporting gene lists in modules

--- RNA_Seq_scripts/Export_Gene_Lists.sh
+++ RNA_Seq_scripts/Export_Gene_Lists.sh
@@ -6,3 +6,5 @@
 #$ -l h=blacklace01.blacklace|blacklace02.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace|blacklace12.blacklace
 
 scripts=/home/adamst/git_repos/scripts/phytophthora_fragariae/RNA_Seq_scripts
+
+/home/adamst/prog/R/R-3.2.5/bin/Rscript --vanilla $scripts/Export_Gene_Lists.R --out_dir $1
