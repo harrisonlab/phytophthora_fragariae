3ab51c76167b01ac6af484ad594b449098c2b15a
Thomas Adams <thomas.adams@emr.ac.uk>
2018-03-15 10:30:14 +0000

    Adds commands to load gene table

--- RNA_Seq_scripts/Export_Gene_Lists.R
+++ RNA_Seq_scripts/Export_Gene_Lists.R
@@ -13,13 +13,18 @@ options(stringsAsFactors = FALSE)
 
 opt_list <- list(
     make_option("--out_dir", type = "character",
-    help = "Directory for output to be written to")
+    help = "Directory for output to be written to"),
+    make_option("--gene_table", type = "character",
+    help = "Input file of RNA-Seq data")
     )
 
 opt <- parse_args(OptionParser(option_list = opt_list))
 outdir <- opt$out_dir
+inp <- opt$gene_table
 
 lfile <- paste(outdir, "Cleaned_data.RData", sep = "/")
 lnames <- load(file = lfile)
 lfile2 <- paste(outdir, "modules.RData", sep = "/")
 lnames2 <- load(file = lfile2)
+
+#
