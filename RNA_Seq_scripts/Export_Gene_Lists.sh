d964ce5d3598303856effccb7776821a6e0b66e9
Thomas Adams <thomas.adams@emr.ac.uk>
2018-03-15 17:00:43 +0000

    Adds scripts variable

--- RNA_Seq_scripts/Export_Gene_Lists.sh
+++ RNA_Seq_scripts/Export_Gene_Lists.sh
@@ -4,3 +4,5 @@
 #$ -pe smp 1
 #$ -l h_vmem=1G
 #$ -l h=blacklace01.blacklace|blacklace02.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace|blacklace12.blacklace
+
+scripts=/home/adamst/git_repos/scripts/phytophthora_fragariae/RNA_Seq_scripts
