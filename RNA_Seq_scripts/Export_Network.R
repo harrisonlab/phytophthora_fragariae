f3ae77b88e368fb90a3e151246d7d7988577363c
Thomas Adams <thomas.adams@emr.ac.uk>
2018-03-15 16:57:19 +0000

    Adds commands to load libraries and load comand line options

--- RNA_Seq_scripts/Export_Network.R
+++ RNA_Seq_scripts/Export_Network.R
@@ -1,1 +1,25 @@
 #!/home/adamst/prog/R/R-3.2.5/Rscript
+
+# Load packages
+
+library("WGCNA")
+library("optparse")
+
+# Import option recommended in WGCNA documentation
+
+options(stringsAsFactors = FALSE)
+
+# Parse arguments
+
+opt_list <- list(
+    make_option("--out_dir", type = "character",
+    help = "Directory for output to be written to")
+    )
+
+opt <- parse_args(OptionParser(option_list = opt_list))
+outdir <- opt$out_dir
+
+lfile <- paste(outdir, "Cleaned_data.RData", sep = "/")
+lnames <- load(file = lfile)
+lfile2 <- paste(outdir, "modules.RData", sep = "/")
+lnames2 <- load(file = lfile2)
