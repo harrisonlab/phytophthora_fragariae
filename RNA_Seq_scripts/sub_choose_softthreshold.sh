2d36cd7ae91fa18e7457ababbed7e565d70b8f30
Thomas Adams <thomas.adams@emr.ac.uk>
2018-03-26 13:49:47 +0100

    Corrects blacklace12 to blacklace11

--- RNA_Seq_scripts/sub_choose_softthreshold.sh
+++ RNA_Seq_scripts/sub_choose_softthreshold.sh
@@ -3,7 +3,7 @@
 #$ -cwd
 #$ -pe smp 4
 #$ -l h_vmem=8G
-#$ -l h=blacklace01.blacklace|blacklace02.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace|blacklace12.blacklace
+#$ -l h=blacklace01.blacklace|blacklace02.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace|blacklace11.blacklace
 
 scripts=/home/adamst/git_repos/scripts/phytophthora_fragariae/RNA_Seq_scripts
 
