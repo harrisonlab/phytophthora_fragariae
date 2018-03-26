5683ae4093903852203ce8a7255c320278ff9e42
Thomas Adams <thomas.adams@emr.ac.uk>
2018-03-26 09:51:34 +0100

    Modifies sge options

--- RNA_Seq_scripts/sub_export_network_cytoscape.sh
+++ RNA_Seq_scripts/sub_export_network_cytoscape.sh
@@ -2,8 +2,8 @@
 #$ -S /bin/bash
 #$ -cwd
 #$ -pe smp 1
-#$ -l h_vmem=1G
-#$ -l h=blacklace01.blacklace|blacklace02.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace|blacklace12.blacklace
+#$ -l h_vmem=6G
+#$ -l h=blacklace01.blacklace|blacklace02.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace|blacklace12.blacklace
 
 scripts=/home/adamst/git_repos/scripts/phytophthora_fragariae/RNA_Seq_scripts
 
