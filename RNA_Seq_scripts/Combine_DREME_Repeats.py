5ed271facdbafab04ff88e2d26709bba4ff81f68
Thomas Adams <thomas.adams@emr.ac.uk>
2018-04-17 16:39:00 +0100

    Adds significance levels to title of script

--- RNA_Seq_scripts/Combine_DREME_Repeats.py
+++ RNA_Seq_scripts/Combine_DREME_Repeats.py
@@ -3,6 +3,7 @@
 '''
 This script will analyse results from each repetition of DREME and extract
 motifs present in 90% of runs
+With significance thresholds of: 0.1, 0.05, 0.01 and 0.001
 '''
 
 import argparse
