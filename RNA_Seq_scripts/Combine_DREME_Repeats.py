85a7a507d87134c2e992995ad1dd5b75527fdf03
Thomas Adams <thomas.adams@emr.ac.uk>
2018-04-17 16:07:22 +0100

    Adds commands to load in command line arguments

--- RNA_Seq_scripts/Combine_DREME_Repeats.py
+++ RNA_Seq_scripts/Combine_DREME_Repeats.py
@@ -12,3 +12,10 @@ from collections import defaultdict
 # Step 1
 # Import variables
 # -----------------------------------------------------
+
+ap = argparse.ArgumentParser()
+ap.add_argument('--inputs', required=True, type=str, help='Lists of files \
+from repeated runs of DREME')
+ap.add_argument(--'output', required=True, type=str, help='Location to output \
+results to')
+conf = ap.parse_args()
