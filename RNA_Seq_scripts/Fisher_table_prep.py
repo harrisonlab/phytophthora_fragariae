a207062328ea7fdc9a6159c53b7b5a9cad666a64
Thomas Adams <thomas.adams@emr.ac.uk>
2018-04-19 13:25:35 +0100

    Adds title explaining purpose of script

--- RNA_Seq_scripts/Fisher_table_prep.py
+++ RNA_Seq_scripts/Fisher_table_prep.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python
+
+'''
+This script will count the number of RxLRs, CRNs, ApoplastP hits and combined
+effector genes in a coexpressed module compared to and the rest of the genome
+'''
