376974edca45e0d274179b022d1c6f6ac77cdd9d
Thomas Adams <thomas.adams@emr.ac.uk>
2018-04-19 13:27:59 +0100

    Adds commands to import libraries

--- RNA_Seq_scripts/Fisher_table_prep.py
+++ RNA_Seq_scripts/Fisher_table_prep.py
@@ -4,3 +4,8 @@
 This script will count the number of RxLRs, CRNs, ApoplastP hits and combined
 effector genes in a coexpressed module compared to and the rest of the genome
 '''
+
+import argparse
+import numpy
+from collections import defaultdict
+import os
