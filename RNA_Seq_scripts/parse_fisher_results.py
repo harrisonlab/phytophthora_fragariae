5b0c8434bdcf037846a7785986862fb48fa8926f
Thomas Adams <thomas.adams@emr.ac.uk>
2018-04-20 10:25:23 +0100

    Adds initial note on purpose of script

--- RNA_Seq_scripts/parse_fisher_results.py
+++ RNA_Seq_scripts/parse_fisher_results.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python
+
+'''
+This script will analyse results from a Fishers Exact Test on enrichment
+for RxLRs, CRNs, ApoplastP hits and combined Effectors vs the genome
+With significance thresholds of: 0.1, 0.05, 0.01 and 0.001
+'''
