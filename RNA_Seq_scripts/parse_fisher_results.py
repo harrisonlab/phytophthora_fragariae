f512675bd779fbdc9e97461be95a880baab0410d
Thomas Adams <thomas.adams@emr.ac.uk>
2018-04-20 10:29:33 +0100

    Adds commands to import python modules

--- RNA_Seq_scripts/parse_fisher_results.py
+++ RNA_Seq_scripts/parse_fisher_results.py
@@ -5,3 +5,7 @@ This script will analyse results from a Fishers Exact Test on enrichment
 for RxLRs, CRNs, ApoplastP hits and combined Effectors vs the genome
 With significance thresholds of: 0.1, 0.05, 0.01 and 0.001
 '''
+
+import argparse
+from collections import defaultdict
+import os
