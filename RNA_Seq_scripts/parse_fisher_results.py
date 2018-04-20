422d63712ac31788fc98c93bc016f42340598411
Thomas Adams <thomas.adams@emr.ac.uk>
2018-04-20 10:47:14 +0100

    Adds loop to create enrichment dictionary

--- RNA_Seq_scripts/parse_fisher_results.py
+++ RNA_Seq_scripts/parse_fisher_results.py
@@ -28,3 +28,17 @@ cwd = os.getcwd()
 # Step 2
 # Creates dictionary of each gene type in each option
 # -----------------------------------------------------
+
+enrichment_dict = defaultdict(float)
+
+Files = conf.inputs
+for File in Files:
+    with open(File) as f:
+        Lines = f.readlines()
+        for line in Lines:
+            split_line = line.split()
+            Module_ID = str(split_line[0])
+            Gene_type = str(split_line[1])
+            P_value = float(split_line[2])
+            key = "_".join([Module_ID, Gene_type])
+            enrichment_dict[key] = P_value
