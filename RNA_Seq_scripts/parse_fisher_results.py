94324f90c6f2a41d7bffe64f9f74fcfb13b5b766
Thomas Adams <thomas.adams@emr.ac.uk>
2018-04-23 09:28:56 +0100

    Corrects line length issue

--- RNA_Seq_scripts/parse_fisher_results.py
+++ RNA_Seq_scripts/parse_fisher_results.py
@@ -51,7 +51,8 @@ for File in Files:
             P_vals.append(P_value)
 
 FDR = conf.FDR
-Corrected_Pval_array = stats.multipletests(P_vals, alpha=FDR, method='fdr_bh', is_sorted=False, returnsorted=False)
+Corrected_Pval_array = stats.multipletests(P_vals, alpha=FDR, method='fdr_bh',
+                                           is_sorted=False, returnsorted=False)
 
 i = 0
 for key in keys:
