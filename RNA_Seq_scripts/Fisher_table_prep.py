baaf9fc683f7638bfb28c3faebbf0044969f5480
Thomas Adams <thomas.adams@emr.ac.uk>
2018-04-23 09:42:08 +0100

    Modifies header

--- RNA_Seq_scripts/Fisher_table_prep.py
+++ RNA_Seq_scripts/Fisher_table_prep.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
 '''
-This script will count the number of RxLRs, CRNs, ApoplastP hits and combined
-effector genes in a coexpressed module compared to and the rest of the genome
+This script will count the number of RxLRs, CRNs, ApoplastP hits, combined
+effector genes and secreted genes in a coexpressed module compared to the rest
+of the genome
 '''
 
 import argparse
