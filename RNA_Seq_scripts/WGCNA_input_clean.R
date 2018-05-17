5b93316fd717c775d1e092cc6ed4d66e3d94fe96
Thomas Adams <thomas.adams@emr.ac.uk>
2018-05-17 11:08:57 +0100

    Modifies script to allow command line specification of FPKM locations

--- RNA_Seq_scripts/WGCNA_input_clean.R
+++ RNA_Seq_scripts/WGCNA_input_clean.R
@@ -15,12 +15,18 @@ opt_list <- list(
   make_option("--gene_table", type = "character",
   help = "Input file of RNA-Seq data"),
   make_option("--out_dir", type = "character",
-  help = "Directory for plots to be written to")
+  help = "Directory for plots to be written to"),
+  make_option("--FPKM_start", type = "integer",
+  help = "Column number for the start of FPKM values"),
+  make_option("--FPKM_end", type = "integer",
+  help = "Column number for the end of FPKM values")
   )
 
 opt <- parse_args(OptionParser(option_list = opt_list))
 inp <- opt$gene_table
 outdir <- opt$out_dir
+FPKM_start <- opt$FPKM_start
+FPKM_end <- opt$FPKM_end
 
 # Load input file
 
@@ -28,9 +34,9 @@ exp_data <- read.csv(inp, sep = "\t")
 
 # Parse data as WGCNA tutorial recommends
 
-datexpr0 <- as.data.frame(t(exp_data[, c(26:33)]))
+datexpr0 <- as.data.frame(t(exp_data[, c(FPKM_start:FPKM_end)]))
 names(datexpr0) <- exp_data$transcript_id
-rownames(datexpr0) <- names(exp_data)[c(26:33)]
+rownames(datexpr0) <- names(exp_data)[c(FPKM_start:FPKM_end)]
 
 # Check for excessive missing values and ID outliers
 
