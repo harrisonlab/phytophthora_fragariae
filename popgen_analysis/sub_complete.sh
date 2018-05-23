87384f56c2629e063ec58a88e1c540e2d9d70828
Thomas Adams <thomas.adams@emr.ac.uk>
2018-05-23 09:59:35 +0100

    Adds shebang & sge instructions

---
+++ popgen_analysis/sub_complete.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -cwd
+#$ -pe smp 1
+#$ -l h_vmem=2G
+#$ -l h=blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace
