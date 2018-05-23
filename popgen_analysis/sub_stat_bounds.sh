ab32fa771fae3b15fa2b23e6adf94c37a6682688
Thomas Adams <thomas.adams@emr.ac.uk>
2018-05-23 17:00:41 +0100

    Copies commands from rates script

---
+++ popgen_analysis/sub_stat_bounds.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -cwd
+#$ -pe smp 1
+#$ -l h_vmem=2G
+#$ -l h=blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace
+
+input=$1
+burn=$2
+loc=$3
+
+stat -input $input -burn $burn -loc $loc
+mv res.txt res_rates.txt
