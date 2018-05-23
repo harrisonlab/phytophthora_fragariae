ab21eabe0cf92baca64399e7d7b37e074aaeff07
Thomas Adams <thomas.adams@emr.ac.uk>
2018-05-23 16:58:28 +0100

    Adds commands to load input options, run stat & change name of output file

--- popgen_analysis/sub_stat_rates.sh
+++ popgen_analysis/sub_stat_rates.sh
@@ -4,3 +4,10 @@
 #$ -pe smp 1
 #$ -l h_vmem=2G
 #$ -l h=blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace
+
+input=$1
+burn=$2
+loc=$3
+
+stat -input $input -burn $burn -loc $loc
+mv res.txt res_rates.txt
