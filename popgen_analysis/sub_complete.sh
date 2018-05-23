28ee93ef1305ab952c3fe21dcbdfdc1d18546d66
Thomas Adams <thomas.adams@emr.ac.uk>
2018-05-23 10:08:23 +0100

    Adds commands to load command line variables

--- popgen_analysis/sub_complete.sh
+++ popgen_analysis/sub_complete.sh
@@ -4,3 +4,9 @@
 #$ -pe smp 1
 #$ -l h_vmem=2G
 #$ -l h=blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace
+
+input_dir=$1
+n=$2
+four_Ner_max=$3
+no_points=$4
+theta=$5
