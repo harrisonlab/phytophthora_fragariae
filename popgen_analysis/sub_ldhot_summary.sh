03eaa796f5aed81fd6f4ba6797efa5f3a29a0378
Thomas Adams <thomas.adams@emr.ac.uk>
2018-05-24 12:01:23 +0100

    Adds command sto load inputs and run ldhot summary

--- popgen_analysis/sub_ldhot_summary.sh
+++ popgen_analysis/sub_ldhot_summary.sh
@@ -4,3 +4,9 @@
 #$ -pe smp 1
 #$ -l h_vmem=2G
 #$ -l h=blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace
+
+res=$1
+hot=$2
+out=$3
+
+ldhot_summary --res $res --hot $hot --out $out
