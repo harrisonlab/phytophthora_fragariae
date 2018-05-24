ef8424d2d0fb69be1bde94de9a796d30a6beeef4
Thomas Adams <thomas.adams@emr.ac.uk>
2018-05-24 11:26:47 +0100

    Adds commands to load input options and run LDhot

--- popgen_analysis/sub_ldhot.sh
+++ popgen_analysis/sub_ldhot.sh
@@ -4,3 +4,12 @@
 #$ -pe smp 1
 #$ -l h_vmem=2G
 #$ -l h=blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace
+
+seq=$1
+loc=$2
+lk=$3
+res=$4
+nsim=$5
+out=$6
+
+ldhot --seq $seq --loc $loc --lk $lk --res $res --nsim $nsim --out $out
