eaf2186368d7d27f47809d068088d26815330c51
Thomas Adams <thomas.adams@emr.ac.uk>
2018-06-04 17:03:37 +0100

    Adds commands to specify options

--- popgen_analysis/plot_LD.R
+++ popgen_analysis/plot_LD.R
@@ -4,3 +4,14 @@
 
 library("ggplot2")
 library("optparse")
+
+# Parse arguments
+
+opt_list <- list(
+    make_option("--out_file", type = "character",
+    help = "Output file for figure to be written to"),
+    make_option("--res_in", type = "character",
+    help = "File containing the mean recombination rates at different loci"),
+    make_option("--hotspot_in", type = "character",
+    help = "File containing the locations of recombination hotspots")
+)
