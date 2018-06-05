77fc9e05f4b0300bbc49edb76e9031bb8a0198c9
Thomas Adams <thomas.adams@emr.ac.uk>
2018-06-05 11:41:39 +0100

    Corrects use of kb in inputs and corrects drawing rectangles

--- popgen_analysis/plot_LD.R
+++ popgen_analysis/plot_LD.R
@@ -28,6 +28,7 @@ Hotspot_df <- read.table(Hotspot_file)
 Recomb_df_corrected <- Recomb_df[-c(1), ]
 Hotspot_df$V1 <- Hotspot_df$V1 * 1000
 Hotspot_df$V2 <- Hotspot_df$V2 * 1000
+Recomb_df_corrected$Loci <- Recomb_df_corrected$Loci * 1000
 
 # Plot graph
 
@@ -35,7 +36,7 @@ Rho_plot <- ggplot(data = Recomb_df_corrected,
     aes(x = Recomb_df_corrected$Loci, y = Recomb_df_corrected$Mean_rho,
     group = 1)) + geom_line() + geom_point() +
     labs(x = "Contig position (bp)", y = "Mean recombination rate") +
-    geom_rect(data = Hotspot_df,
+    geom_rect(data = Hotspot_df, inherit.aes = FALSE,
         aes(xmin = Hotspot_df$V1, xmax = Hotspot_df$V2,
             ymin = -10, ymax = -1))
 
