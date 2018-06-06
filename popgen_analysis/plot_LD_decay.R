6325d55be2b1226013b86839704b37eeb1fcd0b1
Thomas Adams <thomas.adams@emr.ac.uk>
2018-06-06 10:13:03 +0100

    Adds commands to specify options

--- popgen_analysis/plot_LD_decay.R
+++ popgen_analysis/plot_LD_decay.R
@@ -3,3 +3,13 @@
 # Load libraries
 
 library("optparse")
+
+opt_list <- list(
+    make_option("--out_file", type = "character",
+    help = "Output file for figure to be written to"),
+    make_option("--Chromosome_number", type = "integer",
+    help = "The number of chromosomes (or contigs) sampled"),
+    make_option("--LD_statistics", type = "character",
+    help = "Output from vcftools LD statistics calling, contains r^2
+    and positions of variants")
+)
