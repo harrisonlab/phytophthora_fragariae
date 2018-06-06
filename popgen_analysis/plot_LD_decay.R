aeda2aea57eb83ebcb70b073cb64f6dc3e23f27d
Thomas Adams <thomas.adams@emr.ac.uk>
2018-06-06 11:43:04 +0100

    Removes periods from variable names and replace with underscores

--- popgen_analysis/plot_LD_decay.R
+++ popgen_analysis/plot_LD_decay.R
@@ -32,32 +32,32 @@ LD_data <- c(input$R.2)
 # Following code from:
 # https://fabiomarroni.wordpress.com/2011/08/09/estimate-decay-of-linkage-disequilibrium-with-distance/
 
-HW.st <- c(C = 0.1)
-HW.nonlinear <- nls(LD_data~ ( (10 + C * distance) / ( (2 + C *
+HW_st <- c(C = 0.1)
+HW_nonlinear <- nls(LD_data~ ( (10 + C * distance) / ( (2 + C *
     distance) * (11 + C * distance))) * (1 + ( (3 + C * distance) * (12 +
         12 * C * distance + (C * distance) ^ 2)) / (n * (2 + C *
-            distance) * (11 + C * distance))), start = HW.st,
+            distance) * (11 + C * distance))), start = HW_st,
             control = nls.control(maxiter = 100))
-tt <- summary(HW.nonlinear)
-new.rho <- tt$parameters[1]
-fpoints <- ( (10 + new.rho * distance) / ( (2 + new.rho * distance) * (11 +
-    new.rho * distance))) * (1 + ( (3 + new.rho * distance) * (12 + 12 *
-        new.rho * distance + (new.rho * distance) ^ 2)) / (n * (2 + new.rho *
-            distance) * (11 + new.rho * distance)))
+tt <- summary(HW_nonlinear)
+new_rho <- tt$parameters[1]
+fpoints <- ( (10 + new_rho * distance) / ( (2 + new_rho * distance) * (11 +
+    new_rho * distance))) * (1 + ( (3 + new_rho * distance) * (12 + 12 *
+        new_rho * distance + (new_rho * distance) ^ 2)) / (n * (2 + new_rho *
+            distance) * (11 + new_rho * distance)))
 
 # Calculate the half-decay distance
 
 df <- data.frame(distance, fpoints)
-maxld <- max(LD.data)
+maxld <- max(LD_data)
 half_decay <- maxld / 2
-half_decay_distance <- df$distance[which.min(abs(df$fpoints - h.decay))]
+half_decay_distance <- df$distance[which.min(abs(df$fpoints - half_decay))]
 cat("Half decay distance of LD r^2: ", half_decay_distance)
 
 # Plot LD points and a dotted line
 
 pdf(outfile)
 ld_df <- data.frame(distance, fpoints)
-ld_df <- ld.df[order(ld.df$distance), ]
-plot(distance, LD.data, pch = 19, cex = 0.9)
-lines(ld.df$distance, ld.df$fpoints, lty = 3, lwd = 1.2)
+ld_df <- ld_df[order(ld_df$distance), ]
+plot(distance, LD_data, pch = 19, cex = 0.9)
+lines(ld_df$distance, ld_df$fpoints, lty = 3, lwd = 1.2)
 dev.off()
