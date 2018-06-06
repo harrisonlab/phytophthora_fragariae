a0c721d1a867401fe57ab580c4bdb949e350b0b2
Thomas Adams <thomas.adams@emr.ac.uk>
2018-06-06 10:44:32 +0100

    Adds comments on fitting results and link to code source

--- popgen_analysis/plot_LD_decay.R
+++ popgen_analysis/plot_LD_decay.R
@@ -27,3 +27,6 @@ input$Distance <- input$POS2 - input$POS1
 distance <- c(input$Distance)
 
 r_sqd <- c(input$R.2)
+
+# Fit results to decomposition model
+# Code from https://fabiomarroni.wordpress.com/2011/08/09/estimate-decay-of-linkage-disequilibrium-with-distance/
