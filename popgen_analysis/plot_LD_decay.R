56c04d4fa3a02939a655f248faeaa9cdcbcca6c6
Thomas Adams <thomas.adams@emr.ac.uk>
2018-06-06 10:16:18 +0100

    Adds commands to assign input files to variables

--- popgen_analysis/plot_LD_decay.R
+++ popgen_analysis/plot_LD_decay.R
@@ -13,3 +13,8 @@ opt_list <- list(
     help = "Output from vcftools LD statistics calling, contains r^2
     and positions of variants")
 )
+
+opt <- parse_args(OptionParser(option_list = opt_list))
+outfile <- opt$out_file
+Chrom_Num <- opt$Chromosome_number
+LD_file <- opt$LD_statistics
