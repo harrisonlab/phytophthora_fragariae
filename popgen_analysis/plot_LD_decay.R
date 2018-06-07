3c8b8ca58922f32330cfeae627881bb0d9b6eaee
Thomas Adams <thomas.adams@emr.ac.uk>
2018-06-07 10:59:32 +0100

    Adds commands to fit data to a model and modifies header names to remove infix operators

--- popgen_analysis/plot_LD_decay.R
+++ popgen_analysis/plot_LD_decay.R
@@ -46,6 +46,11 @@ data <- c(tapply(input$R.2, cut(input$Distance, seq(0, window_size,
     by = bin_size)), mean))
 data_df <- data.frame(data)
 setDT(data_df, keep.rownames = TRUE)
-colnames(data_df) <- c("R^2", "Distance")
+colnames(data_df) <- c("Rsqd", "Distance")
 
 # Fit binned data to Hills and Weir decay function (a non-linear model)
+
+modelC <- nls(Rsqd ~ ( (10 + C * Distance) / ( (2 + C * Distance) * (11 + C *
+    Distance))) * (1 + ( (3 + C * Distance) * (12 + 12 * C * Distance + (C *
+        Distance) ^ 2)) / (n * (2 + C * Distance) * (11 + C * Distance))),
+        data = data_df, start = Cstart, control = nls.control(maxiter = 100))
