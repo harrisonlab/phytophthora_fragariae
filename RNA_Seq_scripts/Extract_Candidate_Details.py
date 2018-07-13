a0c1418f6593b780fe035f72670a8c868f316385
Thomas Adams <thomas.adams@emr.ac.uk>
2018-07-13 10:52:10 +0100

    Adds title of script

--- RNA_Seq_scripts/Extract_Candidate_Details.py
+++ RNA_Seq_scripts/Extract_Candidate_Details.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python
+
+'''
+This script uses candidates identified by Call_Candidate_Confidence.py and
+extracts details from tables created by pacbio_anntoation_tables_modified.py
+or pacbio_anntoation_tables_modified_no_coexp.py depending on isolate examined
+'''
