23a6ab7be21205a7853fcf4b4bec5eee7aa4342a
Thomas Adams <thomas.adams@emr.ac.uk>
2018-07-13 11:54:14 +0100

    Removes unecessary evocation of sys

--- RNA_Seq_scripts/Extract_Candidate_Details.py
+++ RNA_Seq_scripts/Extract_Candidate_Details.py
@@ -6,7 +6,6 @@ extracts details from tables created by pacbio_anntoation_tables_modified.py
 or pacbio_anntoation_tables_modified_no_coexp.py depending on isolate examined
 '''
 
-import sys
 import argparse
 import os
 
