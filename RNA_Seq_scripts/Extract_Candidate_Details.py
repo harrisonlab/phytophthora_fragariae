dacd6f51222347ad0a79ebbd217c26e2a7864cfe
Thomas Adams <thomas.adams@emr.ac.uk>
2018-07-13 14:49:56 +0100

    Moves score to the end of the file not the start, looks weird

--- RNA_Seq_scripts/Extract_Candidate_Details.py
+++ RNA_Seq_scripts/Extract_Candidate_Details.py
@@ -51,12 +51,12 @@ with open(annotation_table) as f:
     annotation_lines = f.readlines()
     annotation_set = set(annotation_lines)
     Header = annotation_lines[0]
-    Header = "\t".join(["Score", Header])
+    Header = "\t".join([Header, "Score"])
     for transcript_ID in transcripts:
         for annotation_line in annotation_set:
             if annotation_line.startswith(transcript_ID):
                 score = score_dict[transcript_ID]
-                Final_line = "\t".join([score, annotation_line])
+                Final_line = "\t".join([annotation_line, score])
                 Output_lines.append(Final_line)
 
 print("Lines of annotation table to keep identified")
