12c618561a013befb5c931ec7c03033682df89aa
Thomas Adams <thomas.adams@emr.ac.uk>
2018-07-13 11:13:35 +0100

    Adds commands to specify arguments

--- RNA_Seq_scripts/Extract_Candidate_Details.py
+++ RNA_Seq_scripts/Extract_Candidate_Details.py
@@ -9,3 +9,14 @@ or pacbio_anntoation_tables_modified_no_coexp.py depending on isolate examined
 import sys
 import argparse
 import os
+
+ap = argparse.ArgumentParser()
+ap.add_argument('--candidates', required=True, type=str,
+                help='TSV file of candidate genes explaning the phenotypic \
+                differences')
+ap.add_argument('--annotation_table', required=True, type=str,
+                help='TSV file containing gene expression details and other \
+                information')
+ap.add_argument('--out_dir', required=True, type=str,
+                help='Directory to write output files to')
+conf = ap.parse_args()
