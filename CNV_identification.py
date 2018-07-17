f0f9535e38f588a5c9876525b51dde3ea0ec5a55
Thomas Adams <thomas.adams@emr.ac.uk>
2018-07-17 14:46:46 +0100

    Adds details on purpose of script

--- CNV_identification.py
+++ CNV_identification.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python
+
+'''
+This script uses a selection of tables created using samtools depth of average
+read depth for each gene in a reference genome when reads from other isolates
+are aligned. This script identifies genes showing high levels of copy number
+variation and writes them to a tsv
+'''
