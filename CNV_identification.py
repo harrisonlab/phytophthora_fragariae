579fa3337f9a69312b2ef70cf6aafccf0bc71401
Thomas Adams <thomas.adams@emr.ac.uk>
2018-07-17 14:58:23 +0100

    Adds commands to specify & parse command line arguments

--- CNV_identification.py
+++ CNV_identification.py
@@ -2,11 +2,34 @@
 
 '''
 This script uses a selection of tables created using samtools depth of average
-read depth for each gene in a reference genome when reads from other isolates
-are aligned. This script identifies genes showing high levels of copy number
-variation and writes them to a tsv
+read depth for each gene in a reference genome when reads from two other
+isolates and itself are aligned. This script identifies genes showing high
+levels of copy number variation and writes them to a tsv
 '''
 
 import argparse
 from collections import defaultdict
 import os
+
+ap = argparse.ArgumentParser()
+ap.add_argument('--Org1_ID', required=True, type=str, help='Name of organism 1 \
+                ')
+ap.add_argument('--Org1_depth', required=True, type=str, help='Text table \
+                containing the average read depth of genes in organism 1 \
+                sequencing reads')
+ap.add_argument('--Org2_ID', required=True, type=str, help='Name of organism 2 \
+                ')
+ap.add_argument('--Org2_depth', required=True, type=str, help='Text table \
+                containing the average read depth of genes in organism 2 \
+                sequencing reads')
+ap.add_argument('--Org3_ID', required=True, type=str, help='Name of organism 3 \
+                ')
+ap.add_argument('--Org3_depth', required=True, type=str, help='Text table \
+                containing the average read depth of genes in organism 3 \
+                sequencing reads')
+ap.add_argument('--gene_fasta', required=True, type=str, help='Fasta file \
+                containing the gene sequences of the referene assembly, \
+                required for normalising by GC content')
+ap.add_argument('--OutDir', required=True, type=str, help='Output directory for \
+                results tsv to be written to')
+conf = ap.parse_args()
