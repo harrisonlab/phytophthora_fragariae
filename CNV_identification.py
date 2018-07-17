973fb0bfbf5168fedc8d89ef7f3be0f7dbeb68f3
Thomas Adams <thomas.adams@emr.ac.uk>
2018-07-17 14:47:39 +0100

    Adds commands to load python modules

--- CNV_identification.py
+++ CNV_identification.py
@@ -6,3 +6,7 @@ read depth for each gene in a reference genome when reads from other isolates
 are aligned. This script identifies genes showing high levels of copy number
 variation and writes them to a tsv
 '''
+
+import argparse
+from collections import defaultdict
+import os
