f2a20b1a358e65a6ec6705c9c30f8b4fa89ab7e6
Thomas Adams <thomas.adams@emr.ac.uk>
2018-07-20 11:17:49 +0100

    Adds commands to load python modules

--- Annotate_CNV_details.py
+++ Annotate_CNV_details.py
@@ -5,3 +5,7 @@ This script takes the results of CNV_identification.py and uses lists of genes
 showing CNV of particular types, identifies which genome has a change in copy
 number and assigns gene classes
 '''
+
+import argparse
+from collections import defaultdict
+import os
