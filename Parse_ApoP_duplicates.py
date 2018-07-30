5a6c4fc153a358f7ef07ecbe1ed386b36a78b16e
Thomas Adams <thomas.adams@emr.ac.uk>
2018-07-30 10:57:16 +0100

    Adds commands to specify command line arguments

--- Parse_ApoP_duplicates.py
+++ Parse_ApoP_duplicates.py
@@ -9,3 +9,12 @@ prediction.
 import argparse
 from collections import defaultdict
 import os
+
+ap = argparse.ArgumentParser()
+ap.add_argument('--gff_in', required=True, type=str, help='input gff file')
+ap.add_argument('--gff_out', required=True, type=str, help='output gff file')
+ap.add_argument('--Aug_ApoP', required=True, type=str, help='File of Augustus \
+ApoplastP predictions')
+ap.add_argument('--ORF_ApoP', required=True, type=str, help='File of ORF \
+ApoplastP predictions')
+conf = ap.parse_args()
