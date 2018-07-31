f4e65aa33bce84d33ca41e9c76c864a126f3282e
Thomas Adams <thomas.adams@emr.ac.uk>
2018-07-31 17:31:14 +0100

    Adds title explaining purpose of script

--- Rename_Effectors.py
+++ Rename_Effectors.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python
+
+'''
+This script renames header files of predicted effectors (and secreted etc.)
+that were used to create final GFFs, in order to match gene names submitted to
+NCBI
+'''
