1a7aa9f095b19efddebf9a384d7514da3198accd
Thomas Adams <thomas.adams@emr.ac.uk>
2018-07-31 17:59:40 +0100

    Adds commands to load old genes into a set

--- Rename_Effectors.py
+++ Rename_Effectors.py
@@ -16,3 +16,20 @@ ap.add_argument('--Conversion_File', type=str, required=True, help='File \
 containing old and new gene IDs')
 ap.add_argument('--Out_File', type=str, required=True, help='File for new gene \
 names predicted as specific effector type')
+conf = ap.parse_args()
+
+# Load features file to a set
+
+Old_Genes = []
+
+with open(conf.Feature_File) as f:
+    lines = f.readlines()
+    for line in lines:
+        stripped_line = line.rstrip()
+        if '.' in stripped_line:
+            Old_Genes.append(stripped_line)
+        else:
+            gene_id = '.'.join([stripped_line, 't1'])
+            Old_Genes.append(gene_id)
+
+Old_Set = set(Old_Genes)
