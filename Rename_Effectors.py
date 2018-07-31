30e4ebdaa1a682cbf69135ae565a98ce10b8b3a8
Thomas Adams <thomas.adams@emr.ac.uk>
2018-07-31 17:31:45 +0100

    Adds commands to import python modues

--- Rename_Effectors.py
+++ Rename_Effectors.py
@@ -5,3 +5,7 @@ This script renames header files of predicted effectors (and secreted etc.)
 that were used to create final GFFs, in order to match gene names submitted to
 NCBI
 '''
+
+from collections import defaultdict
+import os
+import argparse
