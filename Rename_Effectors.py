30aed17505a5a1b7d200193b66eaa2afdcb69eb0
Thomas Adams <thomas.adams@emr.ac.uk>
2018-07-31 17:53:11 +0100

    Adds commands to specify arguments

--- Rename_Effectors.py
+++ Rename_Effectors.py
@@ -9,3 +9,11 @@ NCBI
 from collections import defaultdict
 import os
 import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument('--Feature_File', type=str, required=True, help='File \
+containing predictions of effector genes from older analyses')
+ap.add_argument('--Conversion_File', type=str, required=True, help='File \
+containing old and new gene IDs')
+ap.add_argument('--Out_File', type=str, required=True, help='File for new gene \
+names predicted as specific effector type')
