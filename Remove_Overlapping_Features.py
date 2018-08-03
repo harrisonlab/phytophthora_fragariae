93fc91eb9ae5ec3114fb3f6f44aa4f62b975befc
Thomas Adams <thomas.adams@emr.ac.uk>
2018-08-03 13:39:48 +0100

    Adds commnds to specify arguments

--- Remove_Overlapping_Features.py
+++ Remove_Overlapping_Features.py
@@ -6,3 +6,14 @@ bedtools from the gff
 '''
 
 import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument('--inp_gff', required=True, type=str, help='Gff file for \
+genes to be removed from')
+ap.add_argument('--exclude_list', required=True, type=str, help='Text file \
+of genes to be removed from the gff')
+ap.add_argument('--output_gff', required=True, type=str, help='Gff file with \
+genes in exclude list removed')
+ap.add_argument('--removed_genes', required=True, type=str, help='File \
+containing ID of all removed genes')
+conf = ap.parse_args()
