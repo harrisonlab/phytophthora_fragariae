586b2fe4e7e87c1f9f905f4b6790960b9605f288
Thomas Adams <thomas.adams@emr.ac.uk>
2018-08-19 17:08:00 +0100

    Adds commands to specify command line arguments and parse them

--- Remove_Broken_Genes.py
+++ Remove_Broken_Genes.py
@@ -7,3 +7,15 @@ contig break and there is not an intron at the break
 
 import argparse
 from collections import defaultdict
+
+ap = argparse.ArgumentParser()
+ap.add_argument('--inp_gff', required=True, type=str, help='Gff file for genes \
+to be removed from')
+ap.add_argument('--out_gff', required=True, type=str, help='Output Gff file')
+ap.add_argument('--broken_genes', required=True, type=str, help='Text file of \
+names of genes removed by this script')
+conf = ap.parse_args()
+
+Gff_in = conf.inp_gff
+Gff_out = conf.out_gff
+Broken_out = conf.broken_genes
