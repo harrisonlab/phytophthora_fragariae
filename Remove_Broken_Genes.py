17a6167c64691a9c8c9284b6a2b8cac7a6c5388d
Thomas Adams <thomas.adams@emr.ac.uk>
2018-08-19 17:04:05 +0100

    Adds commands to import modules

--- Remove_Broken_Genes.py
+++ Remove_Broken_Genes.py
@@ -4,3 +4,6 @@
 This script is used to remove features where the front is missing due to a
 contig break and there is not an intron at the break
 '''
+
+import argparse
+from collections import defaultdict
