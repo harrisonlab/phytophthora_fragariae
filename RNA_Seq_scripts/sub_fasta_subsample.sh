f5a32250525a6bd943fe0d807ce38f5d117cbb50
Thomas Adams <thomas.adams@emr.ac.uk>
2018-09-18 13:48:25 +0100

    Increases nodes fasta subsampling allowed on

--- RNA_Seq_scripts/sub_fasta_subsample.sh
+++ RNA_Seq_scripts/sub_fasta_subsample.sh
@@ -3,7 +3,7 @@
 #$ -cwd
 #$ -pe smp 1
 #$ -l h_vmem=1G
-#$ -l h=blacklace01.blacklace|blacklace02.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace
+#$ -l h=blacklace01.blacklace|blacklace02.blacklace|blacklace03.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace
 
 #Input:
 #Fasta file to be subsampled - 1st argument
