4a957a0d32cb9c8d6eddea2a6444e83ba36f4755
Thomas Adams <thomas.adams@emr.ac.uk>
2018-10-05 15:46:31 +0100

    Adds commands to load arguments & import files

--- circos/bedcov_to_circos.py
+++ circos/bedcov_to_circos.py
@@ -11,3 +11,13 @@ This script converts from samtools bedcov format to a circos friendly format
 
 import argparse
 import numpy as np
+
+ap = argparse.ArgumentParser()
+ap.add_argument('--cov_in',required=True,type=str,help='A file output from samtools bedcov')
+ap.add_argument('--per_X_bp',required=False, default=1000, type=float, help='The number of bp over which you want to report the number of features eg. reads per 1000bp')
+
+conf = ap.parse_args()
+multiplier = conf.per_X_bp
+
+with open(conf.cov_in) as f:
+    cov_lines = f.readlines()
