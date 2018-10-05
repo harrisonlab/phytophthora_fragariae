8ad84ae28cb88b16fae23ba80b510df5ccca2075
Thomas Adams <thomas.adams@emr.ac.uk>
2018-10-05 15:45:05 +0100

    Adds commands to import modules

--- circos/bedcov_to_circos.py
+++ circos/bedcov_to_circos.py
@@ -8,3 +8,6 @@ This script converts from samtools bedcov format to a circos friendly format
 # Step 1
 # Import variables, input files and modules
 #-----------------------------------------------------
+
+import argparse
+import numpy as np
