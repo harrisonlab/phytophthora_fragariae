b00a301c30884bbc192684e2c17967f4d3c47ef4
Thomas Adams <thomas.adams@emr.ac.uk>
2018-10-05 15:51:41 +0100

    Adds commands to calculate coverage & parse to circos friendly format

--- circos/bedcov_to_circos.py
+++ circos/bedcov_to_circos.py
@@ -27,3 +27,19 @@ with open(conf.cov_in) as f:
 # Extract feature start and stop positions, and coverage
 # per Kb and print before parsing into circos format.
 #-----------------------------------------------------
+
+for line in cov_lines:
+    line = line.rstrip()
+    split_line = line.split("\t")
+    contig = split_line[0]
+    start = split_line[1]
+    stop = split_line[2]
+    reads = float(split_line[6])
+    lgth = float(stop) - float(start)
+    # print (str(reads) + "\t" + str(lgth))
+    reads_per_bp = np.divide(reads, lgth)
+    # print(reads_per_bp)
+    reads_per_kb = np.multiply(reads_per_bp, multiplier)
+    reads_per_kb = int(np.round_(reads_per_kb, decimals=0,out=None))
+    feature = "\t".join([str(contig), str(start), str(stop), str(reads_per_kb)])
+    print(feature)
