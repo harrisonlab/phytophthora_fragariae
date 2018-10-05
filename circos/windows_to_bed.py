815cf95dfdface52af015e6662439088e0cca0db
Thomas Adams <thomas.adams@emr.ac.uk>
2018-10-05 16:07:40 +0100

    Adds commands to convert format

--- circos/windows_to_bed.py
+++ circos/windows_to_bed.py
@@ -3,3 +3,22 @@
 '''
 This script converts the 100kb windows gff for circos to bed format
 '''
+
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument('--gff_in', required=True, type=str,
+                help='Gff file to be converted to a bed file')
+ap.add_argument('--out_dir', required=True, type=str,
+                help='Output directory to write bed file to')
+conf = ap.parse_args()
+
+with open(conf.gff_in) as f:
+    lines = f.readlines()
+    for line in lines:
+        split_line = line.split()
+        contig = split_line[0]
+        start = split_line[3]
+        stop =  split_line[4]
+        line_out = "\t".join([contig, start, stop])
+        print(line_out)
