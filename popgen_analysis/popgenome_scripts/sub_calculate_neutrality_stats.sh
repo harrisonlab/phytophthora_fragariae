cea58820334ec2871bcd2ca67eaf7eb7e06699eb
Thomas Adams <thomas.adams@emr.ac.uk>
2018-10-15 11:23:18 +0100

    Allows submission to blacklace03

--- popgen_analysis/popgenome_scripts/sub_calculate_neutrality_stats.sh
+++ popgen_analysis/popgenome_scripts/sub_calculate_neutrality_stats.sh
@@ -3,7 +3,7 @@
 #$ -cwd
 #$ -pe smp 1
 #$ -l h_vmem=8G
-#$ -l h=blacklace01.blacklace|blacklace02.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace|blacklace12.blacklace
+#$ -l h=blacklace01.blacklace|blacklace03.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace
 
 scripts=/home/adamst/git_repos/scripts/phytophthora_fragariae/popgen_analysis/popgenome_scripts
 
