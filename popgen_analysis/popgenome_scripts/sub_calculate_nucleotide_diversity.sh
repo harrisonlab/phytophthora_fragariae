ef0a624294d34f28dc2ee74bb324e09ed465b815
Thomas Adams <thomas.adams@emr.ac.uk>
2018-10-15 11:23:18 +0100

    Allows submission to blacklace03

--- popgen_analysis/popgenome_scripts/sub_calculate_nucleotide_diversity.sh
+++ popgen_analysis/popgenome_scripts/sub_calculate_nucleotide_diversity.sh
@@ -3,7 +3,7 @@
 #$ -cwd
 #$ -pe smp 6
 #$ -l h_vmem=4G
-#$ -l h=blacklace01.blacklace|blacklace02.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace|blacklace12.blacklace
+#$ -l h=blacklace01.blacklace|blacklace03.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace
 
 scripts=/home/adamst/git_repos/scripts/phytophthora_fragariae/popgen_analysis/popgenome_scripts
 
