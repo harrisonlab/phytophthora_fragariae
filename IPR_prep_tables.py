01494c6c1f8a48c9df29bc6dad70a017f8b4451d
Thomas Adams <thomas.adams@emr.ac.uk>
2018-10-17 20:36:35 +0100

    Adds title for script

--- IPR_prep_tables.py
+++ IPR_prep_tables.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python
+
+'''
+This script extracts Interproscan annotations for genes and builds Fisher
+contigency tables showing number of annotated genes in a specified set
+in comparison to a second set of genes.
+'''
