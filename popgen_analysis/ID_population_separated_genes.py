8f05377de219f2e85f97ea3826dae6bcdd66921f
Thomas Adams <thomas.adams@emr.ac.uk>
2018-10-17 10:55:54 +0100

    Adds commands to parse arguments

--- popgen_analysis/ID_population_separated_genes.py
+++ popgen_analysis/ID_population_separated_genes.py
@@ -13,9 +13,9 @@ import os
 ap = argparse.ArgumentParser()
 ap.add_argument('--Fst_File', required=True, type=str, help='Tab separated text \
 file of Fst values per gene')
-ap.add_argument('--Kst_file', required=True, type=str, help='Tab separated text \
+ap.add_argument('--Kst_File', required=True, type=str, help='Tab separated text \
 file of Kst values per gene')
-ap.add_argument('--Dxy_file', required=True, type=str, help='Tab separated text \
+ap.add_argument('--Dxy_File', required=True, type=str, help='Tab separated text \
 file of Dxy values per gene')
 ap.add_argument('--Out_Dir', required=True, type=str, help='Output directory')
 ap.add_argument('--Out_Prefix', required=True, type=str, help='Prefix for output \
@@ -26,3 +26,12 @@ conf = ap.parse_args()
 # Step 1
 # Load input files and build data structures
 # -----------------------------------------------------
+
+Fst_in = conf.Fst_File
+Kst_in = conf.Kst_File
+Dxy_in = conf.Dxy_File
+OutDir = conf.Out_Dir
+OutPre = conf.Out_Prefix
+cwd = os.getcwd()
+
+print("Arguments parsed")
