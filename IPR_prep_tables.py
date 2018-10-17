acac786ad4631350e3cfa7e83aac7509fe495e2c
Thomas Adams <thomas.adams@emr.ac.uk>
2018-10-17 20:38:01 +0100

    Adds commands to import modules

--- IPR_prep_tables.py
+++ IPR_prep_tables.py
@@ -5,3 +5,8 @@ This script extracts Interproscan annotations for genes and builds Fisher
 contigency tables showing number of annotated genes in a specified set
 in comparison to a second set of genes.
 '''
+
+import argparse
+import numpy
+for collections import defaultdict
+from operator import itemgetter
