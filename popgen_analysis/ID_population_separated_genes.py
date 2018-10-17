7fd0f1a52a3f8cb050d06b79971628bbcbffeae8
Thomas Adams <thomas.adams@emr.ac.uk>
2018-10-17 10:31:34 +0100

    Adds note explaining purpose of script

--- popgen_analysis/ID_population_separated_genes.py
+++ popgen_analysis/ID_population_separated_genes.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python
+
+'''
+This script takes table output from Pf_popgenome_analysis.md for calculating
+Fst, Kst and Dxy to determine population separation. Pull out high confidence
+and low confidence genes for further analysis.
+'''
