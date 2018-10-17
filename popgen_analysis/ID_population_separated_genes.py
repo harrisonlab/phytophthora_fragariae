6e8d6a491b4ad491ade90ebc69f79fce56782efd
Thomas Adams <thomas.adams@emr.ac.uk>
2018-10-17 10:49:40 +0100

    Adds commands to specify arguments

--- popgen_analysis/ID_population_separated_genes.py
+++ popgen_analysis/ID_population_separated_genes.py
@@ -9,3 +9,15 @@ and low confidence genes for further analysis.
 import argparse
 from collections import defaultdict
 import os
+
+ap = argparse.ArgumentParser()
+ap.add_argument('--Fst_File', required=True, type=str, help='Tab separated text \
+file of Fst values per gene')
+ap.add_argument('--Kst_file', required=True, type=str, help='Tab separated text \
+file of Kst values per gene')
+ap.add_argument('--Dxy_file', required=True, type=str, help='Tab separated text \
+file of Dxy values per gene')
+ap.add_argument('--Out_Dir', required=True, type=str, help='Output directory')
+ap.add_argument('--Out_Prefix', required=True, type=str, help='Prefix for output \
+file')
+conf = ap.parse_args()
