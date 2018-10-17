3abcefdaccecdabaebec1eaa55d49552fa78de4e
Thomas Adams <thomas.adams@emr.ac.uk>
2018-10-17 10:32:05 +0100

    Adds commands to import modules

--- popgen_analysis/ID_population_separated_genes.py
+++ popgen_analysis/ID_population_separated_genes.py
@@ -5,3 +5,7 @@ This script takes table output from Pf_popgenome_analysis.md for calculating
 Fst, Kst and Dxy to determine population separation. Pull out high confidence
 and low confidence genes for further analysis.
 '''
+
+import argparse
+from collections import defaultdict
+import os
