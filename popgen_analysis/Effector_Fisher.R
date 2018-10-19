87cf3ff4c56beac662e98e5521cd8a85dd81d3d2
Thomas Adams <thomas.adams@emr.ac.uk>
2018-10-19 09:46:10 +0100

    Changes commands to only run the comparisons that make the most sense

--- popgen_analysis/Effector_Fisher.R
+++ popgen_analysis/Effector_Fisher.R
@@ -11,17 +11,14 @@ opt_list <- list(
     table to analyse"),
     make_option("--Output_Directory", type = "character", help = "Directory
     where ouput files are written to"),
-    make_option("--Confidence_ID", type = "character", help = "Confidence level
-    being analysed"),
-    make_option("--Gene_Type", type = "character", help = "Type of gene being
-    analysed")
+    make_option("--Effector_type", type = "character", help = "IPR term being
+    examined for enrichment")
 )
 
 opt <- parse_args(OptionParser(option_list = opt_list))
 Input_Table <- opt$Input_Table
 Output_Directory <- opt$Output_Directory
-Confidence_ID <- opt$Confidence_ID
-Gene_Type <- opt$Gene_Type
+Effector_type <- opt$Effector_type
 
 # Create data structures pre-analysis
 
@@ -30,27 +27,43 @@ Fisher_Table <- read.table(Input_Table, header = FALSE, sep = "\t")
 
 Fisher_Matrix <- matrix(c(Fisher_Table$V2[1], Fisher_Table$V2[2],
 Fisher_Table$V3[1], Fisher_Table$V3[2]), nrow = 2, dimnames = list(
-    Annotation = c(Gene_Type, "Other Genes"), Gene_Set = c(Confidence_ID,
-        "Genome")))
+    Annotation = c(Effector_type, "Other Genes"), Gene_Set = c(
+        "Separated_Genes", "Genome")))
+
+# Run Fishers exact test: one way, which way depending on relative proportions
+
+Spearated_Effectors <- Fisher_Table$V2[1]
+Separated_Genes <- Fisher_Table$V2[2]
+Genome_Effectors <- Fisher_Table$V3[1]
+Genome_Genes <- Fisher_Table$V3[2]
+
+Separated_Ratio <- Spearated_Effectors / (Separated_Genes + Spearated_Effectors)
+Genome_Ratio <- Genome_Effectors / (Genome_Genes + Genome_Effectors)
 
 Out_enriched_up <- paste(Output_Directory, "enriched_up.txt", sep = "/")
 Out_enriched_down <- paste(Output_Directory, "enriched_down.txt", sep = "/")
-Out_enriched_2_way <- paste(Output_Directory, "enriched_2_way.txt", sep = "/")
+Out_enriched_equal <- paste(Output_Directory, "enriched_equal.txt", sep = "/")
 
-results_greater <- fisher.test(Fisher_Matrix, y = NULL, workspace = 200000,
-hybrid = FALSE, control = list(), or = 1, alternative = "greater",
-conf.int = TRUE, conf.level = 0.95, simulate.p.value = FALSE, B = 2000)
-outline <- paste(Confidence_ID, Gene_Type, results_greater$p.value, sep = "\t")
-write(outline, file = Out_enriched_up, append = T)
+if (Separated_Ratio > Genome_Ratio){
+    results <- fisher.test(Fisher_Matrix, y = NULL, workspace = 200000,
+    hybrid = FALSE, control = list(), or = 1, alternative = "greater",
+    conf.int = TRUE, conf.level = 0.95, simulate.p.value = FALSE, B = 2000)
+    outline <- paste(Effector_type, results$p.value, sep = "\t")
+    write(outline, file = Out_enriched_up, append = T)
+}
 
-results_less <- fisher.test(Fisher_Matrix, y = NULL, workspace = 200000,
-hybrid = FALSE, control = list(), or = 1, alternative = "less",
-conf.int = TRUE, conf.level = 0.95, simulate.p.value = FALSE, B = 2000)
-outline <- paste(Confidence_ID, Gene_Type, results_less$p.value, sep = "\t")
-write(outline, file = Out_enriched_down, append = T)
-
-results_2_way <- fisher.test(Fisher_Matrix, y = NULL, workspace = 200000,
-hybrid = FALSE, control = list(), or = 1, alternative = "two.sided",
-conf.int = TRUE, conf.level = 0.95, simulate.p.value = FALSE, B = 2000)
-outline <- paste(Confidence_ID, Gene_Type, results_2_way$p.value, sep = "\t")
-write(outline, file = Out_enriched_2_way, append = T)
+# if (Separated_Ratio < Genome_Ratio){
+#     results <- fisher.test(Fisher_Matrix, y = NULL, workspace = 200000,
+#     hybrid = FALSE, control = list(), or = 1, alternative = "less",
+#     conf.int = TRUE, conf.level = 0.95, simulate.p.value = FALSE, B = 2000)
+#     outline <- paste(IPR_term, results$p.value, sep = "\t")
+#     write(outline, file = Out_enriched_down, append = T)
+# }
+#
+# if (Separated_Ratio == Genome_Ratio){
+#     results <- fisher.test(Fisher_Matrix, y = NULL, workspace = 200000,
+#     hybrid = FALSE, control = list(), or = 1, alternative = "two.sided",
+#     conf.int = TRUE, conf.level = 0.95, simulate.p.value = FALSE, B = 2000)
+#     outline <- paste(IPR_term, results$p.value, sep = "\t")
+#     write(outline, file = Out_enriched_equal, append = T)
+# }
