c8e9562c0d97b0b4bdd2ddeb98399d7e6d6468b3
Thomas Adams <thomas.adams@emr.ac.uk>
2018-12-17 14:03:12 +0000

    Adds commands for additional plot

--- RNA_Seq_scripts/Determine_K.R
+++ RNA_Seq_scripts/Determine_K.R
@@ -6,6 +6,7 @@
 # Load packages
 
 library("optparse")
+library("fpc")
 
 # Parse arguments
 
@@ -13,12 +14,15 @@ opt_list <- list(
     make_option("--in_file", type = "character",
     help = "input file of FPKM values"),
     make_option("--out_file_SSE", type = "character",
-    help = "output of SSE scree plot")
+    help = "output file of SSE scree plot"),
+    make_option("--out_file_pamk", type = "character"),
+    help = "output file of pam plot"
 )
 
 opt <- parse_args(OptionParser(option_list = opt_list))
 inp <- opt$in_file
 out_SSE <- opt$out_file_SSE
+out_pamk <- opt$out_file_pamk
 
 # Load in file and draw SSE plot
 
@@ -28,3 +32,13 @@ for (i in 2:15) wss [i] <- sum(kmeans(exp_data, centers = 1)$withinss)
 pdf(out_SSE)
 plot(1:15, wss, type = "b", xlab = "Number of Clusters",
 ylab = "Within groups sum of squares")
+dev.off()
+
+# Estimate K by partitioning around medoids
+
+pamk.best <- pamk(exp_data)
+cat("number of clusters estimated by optimum average silhouette width:",
+pamk.best$nc, "\n")
+pdf(out_pamk)
+plot(pam(exp_data, pamk.best$nc))
+dev.off()
