572a00e84ff4934af6caa657c5efd341e1086920
Thomas Adams <thomas.adams@emr.ac.uk>
2018-12-17 12:00:39 +0000

    Modifies commands to allow two methods of clustering

--- RNA_Seq_scripts/Plot_SSE_scree.R
+++ RNA_Seq_scripts/Plot_SSE_scree.R
@@ -12,19 +12,19 @@ library("optparse")
 opt_list <- list(
     make_option("--in_file", type = "character",
     help = "input file of FPKM values"),
-    make_option("--out_file", type = "character",
+    make_option("--out_file_SSE", type = "character",
     help = "output of SSE scree plot")
 )
 
 opt <- parse_args(OptionParser(option_list = opt_list))
 inp <- opt$in_file
-out <- opt$out_file
+out_SSE <- opt$out_file_SSE
 
 # Load in file and draw SSE plot
 
 exp_data <- read.csv(inp, header = TRUE, sep = "\t")
 wss <- (nrow(exp_data) - 1) * sum(apply(exp_data, 2, var))
 for (i in 2:15) wss [i] <- sum(kmeans(exp_data, centers = 1)$withinss)
-pdf(out)
+pdf(out_SSE)
 plot(1:15, wss, type = "b", xlab = "Number of Clusters",
 ylab = "Within groups sum of squares")
