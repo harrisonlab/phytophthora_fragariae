a418de70e81d6c127ea10646981a00a2487ac997
Thomas Adams <thomas.adams@emr.ac.uk>
2018-12-17 11:20:18 +0000

    Adds commands to parse arguments

--- RNA_Seq_scripts/Plot_SSE_scree.R
+++ RNA_Seq_scripts/Plot_SSE_scree.R
@@ -3,3 +3,16 @@
 # Load packages
 
 library("optparse")
+
+# Parse arguments
+
+opt_list <- list(
+    make_option("--in_file", type = "character",
+    help = "input file of FPKM values"),
+    make_option("--out_file", type = "character",
+    help = "output of SSE scree plot")
+)
+
+opt <- parse_args(OptionParser(option_list = opt_list))
+inp <- opt$in_file
+out <- opt$out_file
