21b8dd38e526b725057af0e738b5ecb9d8d51c8b
Thomas Adams <thomas.adams@emr.ac.uk>
2018-12-17 11:32:18 +0000

    Adds commands to plot SSE scree plot

--- RNA_Seq_scripts/Plot_SSE_scree.R
+++ RNA_Seq_scripts/Plot_SSE_scree.R
@@ -1,5 +1,8 @@
 #!/home/adamst/prog/R/R-3.2.5/bin/Rscript
 
+# This is based on commands listed in the folowing stack overflow answer
+# https://stackoverflow.com/questions/15376075/cluster-analysis-in-r-determine-the-optimal-number-of-clusters/15376462#15376462
+
 # Load packages
 
 library("optparse")
@@ -16,3 +19,12 @@ opt_list <- list(
 opt <- parse_args(OptionParser(option_list = opt_list))
 inp <- opt$in_file
 out <- opt$out_file
+
+# Load in file and draw SSE plot
+
+exp_data <- read.csv(inp, header = TRUE, sep = "\t")
+wss <- (nrow(exp_data) - 1) * sum(apply(exp_data, 2, var))
+for (i in 2:15) wss [i] <- sum(kmeans(exp_data, centres = 1)$withinss)
+pdf(out)
+plot(1:15, wss, type = "b", xlab = "Number of Clusters",
+ylab = "Within groups sum of squares")
